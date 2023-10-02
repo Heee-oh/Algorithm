c63fd9f3f48ab7d3449072bf4fa2e7a29bc320d2
CHANUK HEO <siai3647@gmail.com>
2023-10-02 23:01:53 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.50 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,14 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
-
-
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];
 
-        ArrayList<Integer> list = new ArrayList<>();
         // keymap의 각 문자에 대한 인덱스를 map에 추가 
         Map<Character, Integer> map = new HashMap<>();
 
@@ -16,7 +12,7 @@ class Solution {
             for(int i = 0; i < s.length(); i++) {
                 char tmp = s.charAt(i);
                 int value = map.getOrDefault(tmp, 200);
-                //System.out.println(tmp + " " + (i+1));
+
                 if(value > i + 1) {
                     map.put(tmp, (i + 1));
                 }
@@ -26,17 +22,17 @@ class Solution {
         // targets의 각 문자를 map.get으로 넣어서 값을 더한다
         int index = 0;
         for(String str : targets) {
-            int count = 0;
+            int count = 0, sum = 0;
             for(char c : str.toCharArray()) {
-                if(map.getOrDefault(c, -1) == -1) {
-                    count = -1;
+                count = map.getOrDefault(c, -1); 
+                if(count == -1) {
+                    sum = -1;
                     break;
-                } else {
-                    count += map.getOrDefault(c, -1);
                 }
+                sum += count;
 
             }
-            answer[index++] = count;
+            answer[index++] = sum;
         }
 
         
