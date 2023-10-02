5172947cd62a3089953dace4eecce8c5cb55626d
CHANUK HEO <siai3647@gmail.com>
2023-10-02 22:49:03 +0900

    [unrated] Title: 대충 만든 자판, Time: 2.06 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+
+
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        // keymap의 각 문자에 대한 인덱스를 map에 추가 
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(String s : keymap) {
+            for(int i = 0; i < s.length(); i++) {
+                char tmp = s.charAt(i);
+                int value = map.getOrDefault(tmp, 200);
+                //System.out.println(tmp + " " + (i+1));
+                if(value > i + 1) {
+                    map.put(tmp, (i + 1));
+                }
+            }
+        }
+
+        // targets의 각 문자를 map.get으로 넣어서 값을 더한다
+        int index = 0;
+        for(String str : targets) {
+            int count = 0;
+            for(char c : str.toCharArray()) {
+                if(map.getOrDefault(c, -1) == -1) {
+                    count = -1;
+                    break;
+                } else {
+                    count += map.getOrDefault(c, -1);
+                }
+                
+            }
+            list.add(count);
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
