4e269a256feb5e30d4bdac75df39bff8a76b1a11
CHANUK HEO <siai3647@gmail.com>
2024-04-02 13:28:33 +0900

    [level 2] Title: 피보나치 수, Time: 4.17 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int[] fibonacci = new int[100001];
+        
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1234567;
+            if (i == n) 
+                break;    
+        }
+        
+            
+        
+        return fibonacci[n];
+    }
+}
