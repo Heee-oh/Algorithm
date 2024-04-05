f7d38e00fdbe6f93ebe742a2d20f5a438a3fee29
CHANUK HEO <siai3647@gmail.com>
2024-04-05 20:25:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,8 +1,9 @@
 class Solution {
     public long solution(int n) {
-        long[] Dp = new long[2001];
+        long[] Dp = new long[n+2];
         Dp[1] = 1;
         Dp[2] = 2;
+        if (n < 3) return Dp[n];
         
         for (int i = 3; i < Dp.length; i++) {
             Dp[i] = (Dp[i-1] + Dp[i-2]) % 1234567;
