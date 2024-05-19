b0d885d59fe1f27d89f97f941edf50b01511106c
CHANUK HEO <siai3647@gmail.com>
2024-05-19 21:32:57 +0900

    Update 의상.java

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,23 +4,16 @@ class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> clothesMap = new HashMap<>();
 
-        
         for (String[] clothesType : clothes) {
             int count = clothesMap.getOrDefault(clothesType[1], 0) + 1;
             clothesMap.put(clothesType[1], count);
         }
         
-        if (clothesMap.size() == 1) {
-            return clothes.length;
-        }
-        
-        int temp = 1;
+        int answer = 1;
         for (Integer cnt : clothesMap.values()) {
-            temp *= cnt + 1;
+            answer *= cnt + 1;
         }
-                
-        
-        
-        return temp - 1;
+
+        return answer - 1;
     }
 }
