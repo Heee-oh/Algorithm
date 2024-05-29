8251d3744c0334338c3891aad5880d19d971046b
CHANUK HEO <siai3647@gmail.com>
2024-05-29 15:45:53 +0900

    [level 2] Title: 방문 길이, Time: 1.67 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dirs) {
+        int currentX = 0, currentY = 0;
+        // 이전X , 이동후 X, 이전Y, 이동후 Y
+        Set<String> set = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+        
+        int count = 0;
+        for (char command : dirs.toCharArray()) {
+            if (command == 'L' && currentX - 1 >= -5) {
+                sb.append(currentX).append(currentX - 1).append(currentY).append(currentY);
+                if (set.contains(sb.toString())) {
+                    sb.delete(0, sb.length());
+                    currentX--;
+                    continue;
+                } 
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                sb.append(currentX - 1).append(currentX).append(currentY).append(currentY);
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                currentX--;
+                count++;
+                    
+            }else if (command == 'D' && currentY - 1 >= -5) {
+                sb.append(currentX).append(currentX).append(currentY).append(currentY - 1);
+                if (set.contains(sb.toString())) {
+                    sb.delete(0, sb.length());
+                    currentY--;
+                    continue;
+                } 
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                sb.append(currentX).append(currentX).append(currentY - 1).append(currentY);
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                currentY--;
+                count++;
+                
+            }else if (command == 'R' && currentX + 1 <= 5) {
+                sb.append(currentX).append(currentX + 1).append(currentY).append(currentY);
+                if (set.contains(sb.toString())) {
+                    sb.delete(0, sb.length());
+                    currentX++;
+                    continue;
+                } 
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                sb.append(currentX + 1).append(currentX).append(currentY).append(currentY);
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                currentX++;
+                count++;
+                    
+            }else if (command == 'U' && currentY + 1 <= 5) {
+                sb.append(currentX).append(currentX).append(currentY).append(currentY + 1);
+                if (set.contains(sb.toString())) {
+                    sb.delete(0, sb.length());
+                    currentY++;
+                    continue;
+                } 
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                sb.append(currentX).append(currentX).append(currentY + 1).append(currentY);
+                set.add(sb.toString());
+                sb.delete(0, sb.length());
+                currentY++;
+                count++;
+            }
+        }
+
+        
+        return  count;
+    }
+}
