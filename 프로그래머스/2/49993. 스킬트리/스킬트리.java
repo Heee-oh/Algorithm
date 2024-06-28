ce1f66d40ad93af10fe9984905346b5ccbeb52bd
CHANUK HEO <siai3647@gmail.com>
2024-06-28 18:59:32 +0900

    [level 2] Title: 스킬트리, Time: 0.14 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -4,36 +4,36 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {        
         HashMap<Character, Integer> map = new HashMap<>();
         int answer = 0, rightSequence = 0;
-        
+
         // 맵 초기화
         for (int i = 0; i < skill.length(); i++) {
             map.put(skill.charAt(i), i);
         }
-        
-        
+
+
         for (int i = 0; i < skill_trees.length; i++) {
             boolean flag = true;
             rightSequence = 0;
             for (int j = 0; j < skill_trees[i].length(); j++) {
                 char skillName = skill_trees[i].charAt(j);
                 int currentSkill = map.getOrDefault(skillName, -1);
-                
+
                 if (currentSkill == -1) {
                   continue;
-                    
+
                 } else if (rightSequence != currentSkill) {
                     rightSequence = 0;
                     flag = false;
                     break;
                 }
-                
+
                 rightSequence++;
             }
-            
+
             if (flag) answer++;
         }
-        
-        
+
+
         return answer;
     }
 }
