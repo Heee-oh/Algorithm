1dac0fbfb74173d1600a95024cf3a76e4688f664
CHANUK HEO <siai3647@gmail.com>
2024-06-30 18:04:51 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         String s = br.readLine();
 
         char[] word = new char[26];
@@ -13,9 +14,10 @@ public class Main {
         }
 
         for (int count : word) {
-            bw.write(count + " ");
+            sb.append(count).append(" ");
         }
-
+        
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
 
