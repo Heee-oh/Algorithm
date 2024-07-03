6f00777154a69a43cf278d1fb530005d27b2ac26
CHANUK HEO <siai3647@gmail.com>
2024-07-03 20:57:13 +0900

    [Bronze I] Title: 더하기 사이클, Time: 144 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -6,11 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        int R = Integer.parseInt(st.nextToken());
-//        int C = Integer.parseInt(st.nextToken());
-//        StringBuilder sb = new StringBuilder(br.readLine());
-//        String originNum = sb.toString();
         int originNum = Integer.parseInt(br.readLine());
         int n = originNum;
         int count = 0;
@@ -20,12 +15,11 @@ public class Main {
            int sum = (n / 10) + (n % 10);
            n = ((n % 10) * 10) +  sum % 10 ;
            count++;
-           
+
         } while (originNum != n);
 
         bw.write(count + "\n");
         bw.flush();
-
         bw.close();
 
     }
