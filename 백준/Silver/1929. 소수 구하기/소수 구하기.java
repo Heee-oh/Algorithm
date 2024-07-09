1f844501b4d3ea14a68b4acb6a286fc05a6f81a4
CHANUK HEO <siai3647@gmail.com>
2024-07-09 18:01:27 +0900

    [Silver III] Title: 소수 구하기, Time: 196 ms, Memory: 18712 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int min = Integer.parseInt(st.nextToken());
+        int max = Integer.parseInt(st.nextToken());
+
+
+        System.out.println(prime(min,max));
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static String prime(int min, int max) {
+        boolean[] arr = new boolean[1000001];
+        StringBuilder sb = new StringBuilder();
+        arr[1] = true;
+        
+        for (int i = 2; i <= max; i++) {
+            for (int j = i * 2; j <= max; j+= i) {
+                if (!arr[j]) {
+                    arr[j] = true;
+                }
+            }
+        }
+
+
+        for (int i = (int) min; i <= max; i++) {
+            if (!arr[i]) {
+                sb.append(i).append("\n");
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+}
