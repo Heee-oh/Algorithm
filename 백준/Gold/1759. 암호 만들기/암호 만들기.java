e0d6ed2423f856a7fae4f32e32cf538ce1829487
CHANUK HEO <siai3647@gmail.com>
2024-07-16 20:41:05 +0900

    [Gold V] Title: 암호 만들기, Time: 136 ms, Memory: 14912 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int L;
+    static int C;
+    static String[] vowel = {"a", "e", "i", "o", "u"};
+    static String[] keyword;
+    static String[] password;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken()); // 암호 구성 자릿수
+        C = Integer.parseInt(st.nextToken()); // 문자 개수
+        st = new StringTokenizer(br.readLine());
+        keyword = new String[C];
+        password = new String[L];
+        visited = new boolean[C];
+        for (int i = 0; i < C; i++) {
+            keyword[i] = st.nextToken();
+        }
+
+        Arrays.sort(keyword);
+
+        dfs(0, -1);
+        System.out.println(sb.toString());
+    }
+
+
+
+
+    private static void dfs(int depth, int pre) {
+        if (depth == L) {
+            if (validVowel()) return;
+
+            // 이상없다면 정상 진행
+            for (String ps : password) {
+                sb.append(ps);
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+
+        for (int i = 0; i < C; i++) {
+            if (!visited[i] && pre < i) {
+                visited[i] = true;
+                password[depth] = keyword[i];
+                dfs(depth + 1, i);
+                visited[i] = false;
+            }
+        }
+
+
+    }
+
+    private static boolean validVowel() {
+        int vowCount = 0;
+        for (String s : password) {
+            // 검증
+            for (String v : vowel) {
+                if (s.equals(v)) {
+                    vowCount++;
+                }
+            }
+
+        }
+        // 전체길이 - 모음의 개수 = 자음의 개수
+        if(L - vowCount < 2 || vowCount == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
