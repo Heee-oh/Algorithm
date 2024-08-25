046351f347c0570c17a37b0a57652adec6ff7063
CHANUK HEO <siai3647@gmail.com>
2024-08-25 14:32:04 +0900

    [level 2] Title: 모음 사전, Time: 1.70 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,45 +1,29 @@
 class Solution {
     int count = 0;
     boolean flag = false;
+    StringBuilder sb = new StringBuilder();
+    
     public int solution(String word) {
-        dfs("", trans(word), 0);
+        dfs(word, 0);
         return count;
     }
     
-    private void dfs(String a, String word, int depth) {
+    private void dfs(String word, int depth) {
 
-        if (a.equals(word)) {
+        if (sb.toString().equals(word)) {
             flag = true;
             return;
         }
 
-        if (depth > 4) {
-            return;
-        }
-
-
-
+        if (depth > 4) return;
+        
         for (int i = 1; i <= 5; i++) {
             if (flag) return;
-
             count++;
-            dfs(a + i, word, depth+1);
+            sb.append("AEIOU".charAt(i - 1));
+            dfs(word, depth+1);
+            sb.deleteCharAt(sb.length() - 1);
         }
-
-
     }
 
-    private String trans(String word) {
-        String s = "";
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == 'A') s += '1';
-            else if (word.charAt(i) == 'E') s += '2';
-            else if (word.charAt(i) == 'I') s += '3';
-            else if (word.charAt(i) == 'O') s += '4';
-            else if (word.charAt(i) == 'U') s += '5';
-
-        }
-
-        return s;
-    }
 }
