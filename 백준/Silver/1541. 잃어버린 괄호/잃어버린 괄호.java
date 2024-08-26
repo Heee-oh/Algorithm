442e8881a60314263448012e7d1531389a76129f
CHANUK HEO <siai3647@gmail.com>
2024-08-26 15:02:51 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String expression = br.readLine();
         String exp = expression.replaceAll("[0-9]", "");
-
         int[] num = new int[exp.length() + 1];
         int idx = 0;
 
@@ -17,27 +17,19 @@ public class Main {
             num[idx++] = Integer.parseInt(n);
         }
 
-        int sum = num[0];
-        int tmp = 0;
+        int sum = num[0], tmp = 0;
         boolean minus = false;
 
         for (int i = 1; i < num.length; i++) {
             if (exp.charAt(i - 1) == '-' ) {
                 minus = true;
             }
-
             if (minus) {
-                tmp += num[i];
+                sum -= num[i];
             } else {
                 sum += num[i];
             }
-
-
         }
-
-
-        if (tmp > 0) sum -= tmp;
-
 
 
         bw.write(sum +"");
