51b234d68e4349c110ce3946abf919dae795b522
CHANUK HEO <siai3647@gmail.com>
2024-08-27 18:57:30 +0900

    [Bronze II] Title: 분산처리, Time: 916 ms, Memory: 16248 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int num = 1;
+            for (int j = 0; j < b; j++) {
+                num *= a;
+                num %= 10;
+            }
+
+            sb.append(num == 0 ? 10 : num).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+}
