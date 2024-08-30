a7da5713cfbb767d51e48c0e891a86f3ade90d31
CHANUK HEO <siai3647@gmail.com>
2024-08-30 19:18:50 +0900

    [Silver II] Title: 나무 자르기, Time: 464 ms, Memory: 119244 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -5,18 +5,17 @@ public class Main {
 
     static long max = 0;
     static int[] trees;
-    static int k;
+    static int m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         // 절단기에 설정할 수 있는 높이는 양의 정수 또는 0
         // 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값
         int n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         trees = new int[n];
 
         st = new StringTokenizer(br.readLine());
@@ -24,14 +23,8 @@ public class Main {
             trees[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 나무의 높이는 10억 이하 || 0
-
+        // M의 높이는 20억 이하
         binarySearch(1, Integer.MAX_VALUE);
-
-
-
-
-
 
         bw.write(max + "");
         bw.flush();
@@ -41,20 +34,23 @@ public class Main {
     private static void binarySearch(long left, long right) {
 
         if (left <= right) {
-
             long sum = 0;
             long mid = (left + right) / 2;
 
             for (int i = 0; i < trees.length; i++) {
+                // 자르는 높이보다 작은 나무는 무시
                 if (trees[i] - mid > 0) {
                     sum +=trees[i] - mid;
                 }
             }
 
-            if (sum >= k) {
+            // 나무를 m미터 보다 많이 얻었다면 자르는 높이를 더 높여서 환경보호 (*ㅅ*)
+            if (sum >= m) {
                 max = Math.max(max, mid);
-                binarySearch(mid + 1, right);
-            } else if (sum < k) {
+                binarySearch(mid + 1, right); // 자르는 높이를 높여서 최소한의 m미터 이상만 가져가기 위해 탐색
+
+                // 자르는 높이가 너무 높기에 높이를 낮춘다.
+            } else if (sum < m) {
                 binarySearch(left, mid - 1);
             }
 
