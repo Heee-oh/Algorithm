5423e89043f18c1ae7e405e166e0bb3ea31f52e4
CHANUK HEO <siai3647@gmail.com>
2024-08-30 10:31:14 +0900

    [Silver II] Title: 색종이 만들기, Time: 152 ms, Memory: 16332 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -35,20 +35,28 @@ public class Main {
 
         int divSize = size / 2;
         int count = 0;
+        boolean flag = false;
 
         for (int i = r; i < r+size; i++) {
             for (int j = c; j < c+size; j++) {
                 if (isBlue != map[i][j]) {
-                    recursion(r, c, divSize, map[r][c]);
-                    recursion(r, c + divSize, divSize, map[r][c + divSize]);
-                    recursion(r + divSize, c, divSize, map[r + divSize][c]);
-                    recursion(r + divSize, c + divSize, divSize, map[r + divSize][c + divSize]);
-                    return;
+                    flag = true;
+                    break;
                 }
             }
+            if (flag) break;
         }
 
-        count++;
+        if (flag) {
+            recursion(r, c, divSize, map[r][c]);
+            recursion(r, c + divSize, divSize, map[r][c + divSize]);
+            recursion(r + divSize, c, divSize, map[r + divSize][c]);
+            recursion(r + divSize, c + divSize, divSize, map[r + divSize][c + divSize]);
+
+        } else {
+            count++;
+        }
+
 
         if (isBlue == 1) {
             blue += count;
