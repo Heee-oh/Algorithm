ad66be0251f071613fd794f33d7efc41a6030f2d
CHANUK HEO <siai3647@gmail.com>
2024-08-30 10:45:57 +0900

    [Silver V] Title: 단어 정렬, Time: 296 ms, Memory: 24712 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,57 +1,31 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
+//        HashSet<String> set = new HashSet<>();
+        TreeSet<String> set = new TreeSet<>((o1, o2) -> o1.length() == o2.length() ?
+                o1.compareTo(o2) : o1.length() - o2.length());
 
-        int words = Integer.parseInt(st.nextToken());
-        ArrayList<String> list = new ArrayList<>();
-
-        for(int i = 0; i < words; i++) {
-            String word = br.readLine();
-            if(!list.contains(word))
-                list.add(word);
-
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
         }
 
-        list.sort(new Comparator<String>() {
 
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length()) {
-                    char[] c1 = o1.toCharArray();
-                    char[] c2 = o2.toCharArray();
-                    for(int i = 0; i < o1.length(); i++) {
-                        if(c1[i] == c2[i]) {
-                            continue;
-                        }else if(c1[i] < c2[i]) {
-                            return -1;
-                        }else{
-                            return 1;
-                        }
-                    }
-
-                }else if(o1.length() > o2.length()) {
-                    return 1;
-                }else {
-                    return -1;
-                }
-                return 0;
-            }
-        });
+        for (String s : set) {
+            sb.append(s).append("\n");
+        }
         
-        for(String wd : list) {
-            bw.write(wd+"\n");
-        }
-  
-         
-        bw.flush(); // 내용 출력
-        bw.close(); // 종료
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
-    
+
 }
