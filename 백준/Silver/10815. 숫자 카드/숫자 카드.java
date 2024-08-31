f3916861857d3a93d9c7fd7d97af716c54091344
CHANUK HEO <siai3647@gmail.com>
2024-08-31 09:03:23 +0900

    [Silver V] Title: 숫자 카드, Time: 816 ms, Memory: 135792 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,35 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        HashMap<Integer, Integer> map = new HashMap<>();
 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] cards = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            map.put(num, i);
+            map.put(num, 1);
         }
-        Arrays.sort(cards);
+        
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < m; i++) {
             int key = Integer.parseInt(st.nextToken());
-            int idx = map.getOrDefault(key, -1);
-
-            sb.append(idx == -1 ? 0 : 1).append(" ");
-
+            int idx = map.getOrDefault(key, 0);
+            sb.append(idx).append(" ");
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
-
-
-
-
 }
