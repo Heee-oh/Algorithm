1990c5a3c51da53fe9ba067622d9f8ef586941d1
CHANUK HEO <siai3647@gmail.com>
2024-09-07 20:39:46 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -6,33 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        pq.add(64);
-        int bar = 0;
         int x = Integer.parseInt(br.readLine());
-
-        while (!pq.isEmpty() && x < 64) {
-            int left = pq.poll();
-            int right = left / 2; // 버릴지 냅둘지 여부
-            bar += right;
-
-            if (bar == x){
-                pq.add(right);
-                break;
-            }
-
-            if (bar >= x) {
-                bar -= right;
-                pq.add(right);
-            }else {
-                pq.add(right);
-                pq.add(right);
-            }
-        }
-
-
-        bw.write(pq.size() + "");
+        int count = Integer.bitCount(x);
+        bw.write(count+ "");
         bw.flush();
         bw.close();
     }
