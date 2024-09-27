082ec5645aabd26d6dc1ddacec915814bf8c5b86
CHANUK HEO <siai3647@gmail.com>
2024-09-27 11:47:59 +0900

    Update 소트인사이드.java
    선택정렬도 추가

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -28,3 +28,54 @@ public class Main {
         bw.close();
     }
 }
+
+
+
+
+// 알고리즘 코테 교재에서는 선택정렬로 하라해서 이것도 올림
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String num = br.readLine();
+
+        int[] arr = new int[num.length()];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = num.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int idx = i;
+
+            for (int j = 1 + i; j < arr.length; j++) {
+                if (arr[j] > arr[idx]) {
+                    idx = j;
+                }
+            }
+
+
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+
+
+        for (int i : arr) {
+            sb.append(i);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
+}
