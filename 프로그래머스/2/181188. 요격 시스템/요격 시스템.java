5da38a6c4466d9094bd0238ee07be65bb619ec06
CHANUK HEO <siai3647@gmail.com>
2024-12-30 16:46:02 +0900

    [level 2] Title: 요격 시스템, Time: 513.58 ms, Memory: 179 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,19 +1,44 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
+    // 2차원 
+    // A 공격 : x 축 평행 직선 형태 정수 쌍(s,e) 형태
+    // B: 특정 x 좌표에서 y축에 수평 발사, 관통 (s, e) 인 s, e 는 요격 불가
+      // 요격 미사일은 실수인 x좌표에서도 발사 가능 -> s, e 도 포함해서 그 사이값으로 요격
+
+    // 문제 : 모든 폭격 미사일을 요격하기 위해 필요한 요격 미사일 수의 최솟값
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (a, b) -> a[0] - b[0]); // x좌표 기준으로 정렬
-        int cnt = 0;
-        int last = -1;
-        for (int i = 0; i < targets.length; i++) {
-            int[] missile = targets[i];
-            if (missile[0] > last) { // 새로운 요격 미사일 필요
-                cnt++;
-                last = missile[1] - 1; // 해당 미사일로 커버되는 범위
-            } else if (missile[1] - 1 < last) { // 더 작은 범위로 커버 가능한 미사일 필요
-                last = missile[1] - 1; // 해당 미사일로 커버되는 범위
+        boolean[] check = new boolean[targets.length];
+        int answer = 0;
+
+        // s 기준으로 오름차순 정렬, 같다면 e 기준으로 오름차순 정렬
+        Arrays.sort(targets, (o1, o2) -> {
+            if (o1[0] - o2[0] == 0) {
+                return o1[1] - o2[1];
+            }else {
+                return o1[0] - o2[0];
             }
+        });
+
+        for (int i = 0; i < targets.length; i++) {
+            // 요격당했다면 다음 미사일로
+            if (check[i]) continue;
+
+            int cutLine = targets[i][1];
+            // 요격할 미사일들 선택
+            for (int j = i + 1; j < targets.length; j++) {
+
+                if (cutLine > targets[j][0]) {
+                    cutLine = Math.min(cutLine, targets[j][1]);
+                    check[j] = true;
+                }else {
+                    break;
+                }   
+            }
+
+            answer++;
         }
-        return cnt;
+
+        return answer;
     }
 }
