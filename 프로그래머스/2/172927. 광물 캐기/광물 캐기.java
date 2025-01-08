6d05d1fdd4e1111491d6d17d28fad2116ddc89fe
CHANUK HEO <siai3647@gmail.com>
2025-01-08 14:47:34 +0900

    [level 2] Title: 광물 캐기, Time: 16.25 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,12 +1,4 @@
 class Solution {
-    
-    /** 최소 피로도, 완전탐색
-    * 선택한 곡괭이는 터질때까지 사용
-    * 광물 5개 캘시 곡괭이는 터짐
-    * 광물은 주어진 순서대로
-    * 광물 모두 캐거나, 곡괭이를 다 쓸때까지 
-    **/ 
-    
     // 0 : 다이아, 1 : 철 , 2: 돌
     int[][] fatigue = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
     int minFatigue = Integer.MAX_VALUE;
@@ -19,30 +11,33 @@ class Solution {
         return minFatigue;
     }
     
+    // 백트래킹
     private void dfs(int[] picks, String[] minerals, int idx, int sumFatigue) {
         
+        // 광물을 모두 캤거나, 곡괭이를 다썼다면 종료
         if (idx - 1 >= minerals.length
             || (picks[0] == 0 && picks[1] == 0 && picks[2] == 0)) {
             
             minFatigue = Math.min(minFatigue, sumFatigue);
             return;
         }
-        
+
+        // 곡괭이 별로 돌아가면서 탐색
         for (int i = 0; i < 3; i++) {
             if (picks[i] == 0) continue;
-            picks[i]--;
-            int tmp = 0;
+            int tmp = 0; // 임시 피로도 저장
+            picks[i]--; // 곡괭이 사용
             
             for (int j = idx; j < idx + 5; j++) {
-                if (minerals.length <= j) break;
+                if (minerals.length <= j) break; // 광물을 다 캤다면 멈추기
                 
                 int mineralIdx = getMineralIdx(minerals[j]);
                 tmp += fatigue[i][mineralIdx];
             }
             
+            // 다음 광물 캐기
             dfs(picks, minerals, idx + 5, sumFatigue + tmp);
-            
-            picks[i]++;
+            picks[i]++; // 곡괭이 회복 (백트래킹을 위해
         }
     }
     
