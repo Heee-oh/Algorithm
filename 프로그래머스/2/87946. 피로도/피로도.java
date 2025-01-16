a74d49eed6196dc1c0ff2407409a9e2f053c9052
CHANUK HEO <siai3647@gmail.com>
2025-01-16 14:22:58 +0900

    [level 2] Title: 피로도, Time: 3.42 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -16,6 +16,8 @@ class Solution {
     
     private void dfs(int k, int[][] dungeons, int cnt) {
         for (int i = 0; i < dungeons.length; i++) {
+            
+            // 방문하지 않았고, 현재 피로도가 던전 최소 필요 피로도보다 같거나 클때
             if (!visited[i] && k >= dungeons[i][0]) {  
                 visited[i] = true;
                 dfs(k - dungeons[i][1], dungeons, cnt + 1);
