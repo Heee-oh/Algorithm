34645ad650cc74f91d76c786d6edb2c5f1699661
CHANUK HEO <siai3647@gmail.com>
2025-01-16 14:41:39 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 88.43 ms, Memory: 228 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,39 +1,28 @@
 import java.util.Stack;
+import java.util.Arrays;
 
 class Solution {
-    
-    // 스택을 이용
-    // idx를 함께 저장
-    // 스택 top 원소 < 뒷수 -> 뒷 큰수 넣고, pop()
-    
+
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
-        Stack<int[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        Arrays.fill(answer, -1);
         
         for (int i = 0; i < numbers.length; i++) {
-            if (stack.isEmpty()) {
-                stack.push(new int[] {numbers[i], i});
+            while (!stack.isEmpty()) {
+                int idx = stack.peek();
                 
-            } else {
-                while (!stack.isEmpty()) {
-                    if (stack.peek()[0] < numbers[i]) {
-                        int idx = stack.pop()[1];
-                        answer[idx] = numbers[i];
-                    } else {
-                        break;
-                    }
+                if ( numbers[idx] < numbers[i]) {
+                    answer[idx] = numbers[i];
+                    stack.pop();
+                    
+                } else {
+                   break;
                 }
-                // 뒷 큰수를 다 처리했다면 현재 number를 스택에 저장
-                stack.push(new int[] {numbers[i], i});
             }
+            stack.push(i);
         }
-        
-        // 뒷 큰수가 존재하지 않은 원소는 -1 처리 
-        while (!stack.isEmpty()) {
-            int idx = stack.pop()[1];
-            answer[idx] = -1;
-        }        
-        
         
         return answer;
     }
