f5d7f8cf839c0809d3caa2f84caf460c115bd892
CHANUK HEO <siai3647@gmail.com>
2025-01-21 16:52:21 +0900

    [level 2] Title: 숫자 변환하기, Time: 89.89 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,29 +2,36 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 class Solution {
-    
+
     boolean[] visited = new boolean[1000001];
     // x -> y
     // x + n || x * 2 || x * 3
     //bfs 최단 경로 
-    
+
     public int solution(int x, int y, int n) {
         int answer = bfs(x,y,n);
-        
+
         return answer;
     }
-    
+
     private int bfs(int x, int y, int n) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, 0});
-        
+
+        if (y % 2 == 0 || y % 3 == 0 || y % n == x || y % n == 0) {
+            q.add(new int[]{x, 0});
+        }
+
         while(!q.isEmpty()) {
-            
+
             int[] current = q.poll();
+
+            if (current[0] > y) continue;
+
             // y값이면 반환하고 종료
-            if (current[0] == y) return current[1];
-            
-            
+            if (current[0] == y) {
+                return current[1];
+            }
+
             if (current[0] + n <= y && !visited[current[0] + n]) {
                 q.add(new int[] {current[0] + n, current[1] + 1});
                 visited[current[0] + n] = true;
@@ -33,18 +40,18 @@ class Solution {
                 q.add(new int[] {current[0] * 2, current[1] + 1});
                 visited[current[0] * 2] = true;
             }
-            
+
             if (current[0] * 3 <= y && !visited[current[0] * 3]) {
                 q.add(new int[] {current[0] * 3, current[1] + 1});
                 visited[current[0] * 3] = true;
             }
 
 
-            
+
         }
-        
-        
+
+
         return -1;
-        
+
     }
 }
