148763d7557938631273ac7741be8d9446460bc1
CHANUK HEO <siai3647@gmail.com>
2025-02-01 10:49:48 +0900

    [level 2] Title: 호텔 대실, Time: 10.48 ms, Memory: 93.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] book_time) {
+        // 최대 룸 개수, 시-분 
+        Integer[] time = new Integer[1001];
+        // 시작 시간 순 정렬
+        Arrays.sort(book_time, (o1, o2) -> o1[0].compareTo(o2[0]));
+        int answer = 0;
+        
+        for (String[] checkOut : book_time) {
+            String[] start = checkOut[0].split(":");
+            String[] end = checkOut[1].split(":");
+            
+            // 분으로 계산
+            int startTime = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
+            int endTime = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
+            
+            for (int i = 0; i < time.length; i++) {
+                // 빈 방이라면
+                if (time[i] == null) {
+                    time[i] = (endTime + 10) ; // 23:59분을 넘기면 다시 00:00으로 
+                    answer++;
+                    break;
+                    
+                    // 방이 채워져 있고, 예약시간이 체크아웃 후 시간이라면
+                } else if (time[i] <= startTime) {
+                    time[i] = (endTime + 10) ;
+                    break;
+                }
+                // 현재 방이 이미 예약자가 있고, 체크아웃 전 시간이라면 다음 방으로 이동
+            }
+        }
+        
+        return answer;
+    }
+}
