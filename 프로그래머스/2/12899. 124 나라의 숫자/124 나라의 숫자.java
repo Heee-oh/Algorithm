25819cb2ab5398d11503d4edd5bec7799c8c8833
CHANUK HEO <siai3647@gmail.com>
2025-02-01 11:32:07 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,23 +1,18 @@
 class Solution {
-    int[] dn = {1,2,4};
+    int[] dn = {1,2,4}; // 124 나라 변환 배열
     
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
     
-        
         while (n > 0) {
-            int idx = n % 3;
-            if (idx == 0) idx = 3;
-            
-            sb.append(dn[idx - 1]);
-            if (idx == 3) {
-                n /= 3;
+            int idx = n % 3; // 3으로 나눈 나머지가 124나라의 변환 인덱스이다.
+            if (idx == 0) {
+                idx = 3;
                 n--;
-            } else {
-                n /= 3;
             }
             
-            
+            sb.append(dn[idx - 1]);
+            n /= 3;
         }
 
         return sb.reverse().toString();
