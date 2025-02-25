ad5a9ac2043526c2830ee0db9169bc5bce50369e
CHANUK HEO <siai3647@gmail.com>
2025-02-25 10:46:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 2097.88 ms, Memory: 615 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int[][] edge) {
+        boolean[][] graph = new boolean[n + 1][n + 1];
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
+        
+        for (int i = 0; i < edge.length; i++) {
+            graph[edge[i][0]][edge[i][1]] = true;
+            graph[edge[i][1]][edge[i][0]] = true;
+        }
+        int max = bfs(graph, visited, dist);
+        
+        return (int)Arrays.stream(dist).filter(x -> max == x).count();
+    }
+    
+    private int bfs(boolean[][] graph, boolean[] visited, int[] dist) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {1, 1});
+        visited[1] = true;
+        int max = 0;
+        
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            
+            for (int i = 1; i < graph.length; i++) {
+                if (!visited[i] && graph[node[0]][i]) {
+                    q.add(new int[] {i, node[1] + 1});
+                    dist[i] = node[1] + 1;
+                    visited[i] = true;
+                    max = Math.max(max, node[1] + 1);
+                }
+            }
+            
+        }
+        return max;
+    }
+}
