f6a206a78935ad48a9ea36826fe19f5593151ed6
CHANUK HEO <siai3647@gmail.com>
2025-02-25 10:54:15 +0900

    [level 3] Title: 가장 먼 노드, Time: 1046.29 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,49 +1,67 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+class Edges {
+    HashSet<Integer> set;
+    Edges() { set = new HashSet<>(); }
+}
 
 class Solution {
-    
-    public int solution(int n, int[][] edge) {
-        boolean[][] graph = new boolean[n + 1][n + 1];
-        boolean[] visited = new boolean[n + 1];
-        int[] dist = new int[n + 1];
-        
-        for (int i = 0; i < edge.length; i++) {
-            graph[edge[i][0]][edge[i][1]] = true;
-            graph[edge[i][1]][edge[i][0]] = true;
+
+    public static int getMin(int[] dist, boolean[] vertices) {
+        int min = Integer.MAX_VALUE;
+        int mIdx = -1;
+
+        for (int i = 1; i < vertices.length; i++) {
+            if (!vertices[i] && dist[i] < min) {
+                min = dist[i];
+                mIdx = i;
+            }
         }
-        
-        int max = bfs(graph, visited, dist);
+        return mIdx;
+    }   
+
+    public int solution(int n, int[][] edge) {
         int answer = 0;
-        
-        for (int distance : dist) {
-            
-            if (distance == max) {
+
+        Edges[] adj = new Edges[n + 1];
+        boolean[] included = new boolean[n + 1];
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        // adj Max size : 400KB
+        for (int[] e : edge) {
+            if (adj[e[0]] == null) adj[e[0]] = new Edges();
+            if (adj[e[1]] == null) adj[e[1]] = new Edges();
+            adj[e[0]].set.add(e[1]);
+            adj[e[1]].set.add(e[0]);
+        }
+
+        // root Node : vertex '1'
+        dist[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            int now = getMin(dist, included);
+            included[now] = true;
+            Iterator<Integer> it = adj[now].set.iterator();
+            while (it.hasNext()) {
+                int next = it.next();
+                if (!included[next])
+                    if (dist[next] > dist[now] + 1)
+                        dist[next] = dist[now] + 1;
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <=n; i++) {
+            if (dist[i] > max) {
+                answer = 1;
+                max = dist[i];
+            } else if (dist[i] == max) {
                 answer++;
             }
         }
-        
+
         return answer;
-    }
-    
-    private int bfs(boolean[][] graph, boolean[] visited, int[] dist) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {1, 1});
-        visited[1] = true;
-        int max = 0;
-        
-        while(!q.isEmpty()) {
-            int[] node = q.poll();
-            
-            for (int i = 1; i < graph.length; i++) {
-                if (!visited[i] && graph[node[0]][i]) {
-                    q.add(new int[] {i, node[1] + 1});
-                    dist[i] = node[1] + 1;
-                    visited[i] = true;
-                    max = Math.max(max, node[1] + 1);
-                }
-            }
-            
-        }
-        return max;
     }
 }
