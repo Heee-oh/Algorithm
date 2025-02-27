dd816d159ba58c9bfb324a62d64eb63e1f6c40ce
CHANUK HEO <siai3647@gmail.com>
2025-02-27 10:27:30 +0900

    [level 3] Title: 부대복귀, Time: 278.46 ms, Memory: 176 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,70 +1,72 @@
 import java.util.*;
 
 class Solution {
-    // n이 10만으로 2중배열은 안됨
-    Map<Integer,ArrayList<Integer>> graphMap = new HashMap<>();
-    Map<Integer, Integer> checkMap = new HashMap<>();
 
-    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+    //최단거리 알고리즘
 
-        // map에 노드 추가
-        for (int i = 0; i < roads.length; i++) {
-            if (!graphMap.containsKey(roads[i][0])) {
-                graphMap.put(roads[i][0], new ArrayList<>());
-            }
+    static int[] dist;
+    static List<Node>[] list;
 
-            if (!graphMap.containsKey(roads[i][1])) {
-                graphMap.put(roads[i][1], new ArrayList<>());
-            }
-            graphMap.get(roads[i][0]).add(roads[i][1]);
-            graphMap.get(roads[i][1]).add(roads[i][0]);
+    public int[] solution(int n, int[][] roads, int[] sources, int destination){
+        int[] answer = new int[sources.length];
+
+        dist = new int[n+1];
+        list = new List[n+1];
+
+        for(int i=1; i<=n; i++) {
+            dist[i] = Integer.MAX_VALUE;
         }
 
-        for (int i = 0; i < sources.length; i++) {
-            checkMap.put(sources[i], i);
+        for(int i=1; i<=n; i++) {
+            list[i] = new ArrayList<>();
         }
 
+        for(int i=0; i<roads.length; i++) {
+            list[roads[i][0]].add(new Node(roads[i][1], 1));
+            list[roads[i][1]].add(new Node(roads[i][0], 1));
+        }
 
+        bfs(destination);
 
-        return bfs(sources, destination);
+        for(int i=0; i<sources.length; i++) {
+            if(dist[sources[i]] == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            } else {
+                answer[i] = dist[sources[i]];
+            }
+        }
+
+        return answer;
     }
 
-    // 역으로 destination에서 끝노드까지 탐색하면서 source 가 있다면 그때그때 추가하는 방식
-    private int[] bfs(int[] sources, int destination) {
-        int[] answer = new int[sources.length];
-        Arrays.fill(answer, -1);
-        int idx = 0;
+    static void bfs(int start) {
+        PriorityQueue<Node> queue = new PriorityQueue<>(
+            (o1, o2) -> o1.u - o2.u);
 
-        // 방문 체크
-        Set<Integer> set = new HashSet<>();
+        dist[start] = 0;
+        queue.offer(new Node(start, dist[start]));
 
+        while(!queue.isEmpty()) {
+            Node now = queue.poll();
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {destination, 0});
-        set.add(destination);
+            for(Node next : list[now.v]) {
 
-        while (!q.isEmpty()) {
-            int[] src = q.poll();
-
-            // 목적지에 다다르면 비용 저장
-            if (checkMap.containsKey(src[0])) {
-                answer[checkMap.get(src[0])] = src[1];
-            }
-
-            // 시작 지점 노드 리스트 찾기
-            ArrayList<Integer> list = graphMap.get(src[0]);
-            if (list == null) continue;
-
-            // BFS 탐색
-            for (int region : list) {
-                if (!set.contains(region)) {
-                    q.add(new int[]{region, src[1] + 1});
-                    set.add(region);
+                if(dist[next.v] > now.u + next.u) {
+                    dist[next.v] = now.u + next.u;
+                    queue.offer(new Node(next.v, dist[next.v]));
                 }
             }
         }
-        
-        return answer;
 
+    }
+
+    static class Node {
+        int v;
+        int u;
+
+        public Node(int v, int u) {
+            this.v = v;
+            this.u = u;
+        }
     }
 }
