b1adc4bfb0c76c016c0db3b4adafdcc02e62147c
CHANUK HEO <siai3647@gmail.com>
2025-02-28 00:03:58 +0900

    [Silver II] Title: 예산, Time: 152 ms, Memory: 15840 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -9,35 +9,58 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] countries = new int[n];
+        int max = 0;
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             countries[i] = Integer.parseInt(st.nextToken());
+            max += countries[i];
         }
 
         int m = Integer.parseInt(br.readLine()); // 총 예산
 
-        // 이진 탐색을 위한 범위 설정
+        // 예산 이진 탐색
         int front = 0, back = Arrays.stream(countries).max().getAsInt();
         int answer = 0;
 
         while (front <= back) {
+
             int mid = (back + front) / 2;
             long tmp = 0;
 
+
             for (int i = 0; i < n; i++) {
-                tmp += Math.min(countries[i], mid);
+
+                if (countries[i] <= mid) {
+                    tmp += countries[i];
+                } else {
+                    tmp += mid;
+                }
+
             }
 
+            // 배정된 예산값이 국가 예산 총액을 넘어가거나,
+            // 모든 요청의 합 보다 배정된 예산값이 크거나 같으면 값 범위를 줄임
             if (tmp > m) {
                 back = mid - 1;
+
+
+                // 배정된 예산값이 국가 예산 보다 적다면 더 큰 상한액을 찾기 위해 값 범위를 키움
             } else {
-                answer = mid; // 가능한 값 갱신
-                front = mid + 1;
+                if (max <= tmp) {
+                    back = mid - 1;
+                } else {
+                    front = mid + 1;
+                }
+
+                answer = mid;
             }
         }
 
-        bw.write(answer + "\n");
+
+        bw.write(answer + "");
         bw.flush();
         bw.close();
     }
