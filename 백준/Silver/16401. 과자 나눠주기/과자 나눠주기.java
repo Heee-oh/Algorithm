9287f565eed021316d0de6f2d4d5ff217132cefd
CHANUK HEO <siai3647@gmail.com>
2025-03-03 10:28:38 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1304 ms, Memory: 126392 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] snacks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            snacks[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(snacks);
+        int front = 1, back = snacks[n-1];
+        int size = -1;
+        while (front <= back) {
+
+            int mid = (front + back) >>> 1;
+            int count = 0;
+
+            // 과자 나누기
+            for (int snack : snacks) {
+                count += snack / mid;
+            }
+
+            // 나눈 과자 개수가 조카 M명보다 많거나 같다면 과자길이를 더 늘림
+            if (count >= m) {
+                size = mid;
+                front = mid + 1;
+                // 나눈 과자 개수가 조카 M명보다 적다면 과자 길이를 줄임
+            } else {
+                back = mid - 1;
+            }
+        }
+
+        System.out.println(size == -1 ? 0 : size);
+
+
+
+    }
+
+
+
+}
