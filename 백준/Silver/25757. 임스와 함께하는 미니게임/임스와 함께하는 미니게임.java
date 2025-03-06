ab121d6285f41a0166f128ce5aed782cd154518e
CHANUK HEO <siai3647@gmail.com>
2025-03-06 10:11:14 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 248 ms, Memory: 27348 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Set<String> playerCheck = new HashSet<>();
+        int n = Integer.parseInt(st.nextToken());
+        char gameCode = st.nextToken().charAt(0);
+
+
+        for (int i = 0; i < n; i++) {
+            String player = br.readLine();
+            playerCheck.add(player);
+        }
+
+        int count = getPlayerCount(gameCode) - 1;
+        int playCount = playerCheck.size() / count;
+
+        bw.write(playCount + "");
+        bw.flush();
+        bw.close();
+    }
+
+    private static int getPlayerCount(char code) {
+        switch (code) {
+            case 'Y' : return 2;
+            case 'F' : return 3;
+            default : return 4;
+        }
+    }
+
+}
