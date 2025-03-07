f1d5a3d68239deb60effba4295cc275a0474904f
CHANUK HEO <siai3647@gmail.com>
2025-03-07 21:31:48 +0900

    [Gold V] Title: 리모컨, Time: 1072 ms, Memory: 284316 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static int min = 500001;
+    static int len;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String target = br.readLine();
+        int n = Integer.parseInt(target);
+        int m = Integer.parseInt(br.readLine());
+        boolean[] brokenBtn = new boolean[10];
+
+        if (m > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < m; i++) {
+                int idx = Integer.parseInt(st.nextToken());
+                brokenBtn[idx] = true;
+            }
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (!brokenBtn[i]) {
+                list.add(i);
+            }
+        }
+
+        for (int i : list) {
+            backTracking(n, 1, target.length(), i);
+        }
+
+        bw.write(Math.min(Math.abs(100 - n), min + len) + "");
+        bw.flush();
+        bw.close();
+    }
+
+
+    private static void backTracking(int target, int depth, int size, int num) {
+
+        // size +2 는 범위를 벗어나므로 종료
+        if (depth == size + 2) return;
+
+        //  size보다 1작은 길이의 숫자 혹은 길이가 1큰 숫자도 고려해야함
+        if (size - 1 <= depth && depth <= size + 1) {
+            int abs = Math.abs(num - target);
+            int len = String.valueOf(num).length();
+            min = Math.min(abs + len, min);
+        }
+
+        for (int n : list) {
+            backTracking(target, depth + 1, size, num * 10 + n);
+        }
+    }
+}
+
+
