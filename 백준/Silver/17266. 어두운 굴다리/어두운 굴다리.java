3438c2524e061c4eee13fc31e4c2a12fb03f209e
CHANUK HEO <siai3647@gmail.com>
2025-03-09 13:45:13 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 320 ms, Memory: 27320 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -2,44 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 1팀 = 6명 (상위 4명 점수 합)
-    // 결승점을 통과한 순서대로 점수 받음
-    // 가장 낮은 점수 팀이 우승
-    // 6명 주자 참가못한 팀은 제외
-    // 동점일 경우 5번째 사람 가장빨리 들어온 팀이 우승
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Map<Integer, Integer> teamCheck = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        // 다리 길이
-        boolean[] bridge = new boolean[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         ArrayList<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < m; i++) {
             int lampIdx = Integer.parseInt(st.nextToken());
             list.add(lampIdx);
         }
 
-
         int front = 0, back = n;
         int min = 100001;
 
         while (front <= back) {
-            int loadStart = 100001;
-            int loadEnd = 0;
-
+            
             int mid = (front + back) >>> 1;
 
-            for (int lamp : list) {
-                int left = lamp - mid;
-                loadStart = Math.min(loadStart, left); // 맨 처음 시작부분 체크
+            int loadStart = list.get(0) - mid; // 빛이 비춰지는 처음 시작 위치
+            int loadEnd = 0; // 빛이 비춰지는 마지막 위치
 
-                // 이전 길 끝부분이 현재 왼쪽 비친부분 안이면 갱신
+            
+            for (int lamp : list) {
+                int left = lamp - mid; // 가로등의 왼쪽 비추는 부분
+
+                // 현재 왼쪽 비친부분이 이전 비춰지는 마지막 위치안이라면 현재 비치는 마지막부분으로 갱신   
                 if (left <= loadEnd) {
                     loadEnd = lamp + mid;
                 }
@@ -54,9 +45,6 @@ public class Main {
                 front = mid + 1;
             }
         }
-
-
-
 
         bw.write(min + "");
         bw.flush();
