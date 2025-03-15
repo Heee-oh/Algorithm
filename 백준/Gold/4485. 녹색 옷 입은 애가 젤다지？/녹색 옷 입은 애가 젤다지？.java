398ddbadad63d3da305f84136e350b4a68256bd7
CHANUK HEO <siai3647@gmail.com>
2025-03-15 10:02:52 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 680 ms, Memory: 298252 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int[][] map;
+    static int[][] diff;
+    static int min;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
+
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+
+            min = Integer.MAX_VALUE;
+            map = new int[n][n];
+            diff = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(diff[i], Integer.MAX_VALUE);
+            }
+
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            bfs();
+
+            sb.append("Problem ").append(cnt++).append(": ").append(diff[n-1][n-1]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, map[0][0]});
+        diff[0][0] = map[0][0];
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nextR = cur[0] + dy[i];
+                int nextC = cur[1] + dx[i];
+
+                if (nextR < 0 || nextR >= map.length || nextC < 0 || nextC >= map.length) continue;
+
+                if (diff[nextR][nextC] > cur[2] + map[nextR][nextC]) {
+                    diff[nextR][nextC] = cur[2] + map[nextR][nextC];
+                    q.add(new int[]{nextR, nextC, diff[nextR][nextC]});
+                }
+            }
+        }
+
+    }
+
+}
