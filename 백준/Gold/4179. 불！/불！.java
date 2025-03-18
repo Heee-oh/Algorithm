531e912d5e80c3a1320a03bb01f1e0e563c149ab
CHANUK HEO <siai3647@gmail.com>
2025-03-18 13:54:51 +0900

    [Gold III] Title: 불!, Time: 552 ms, Memory: 72388 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -6,7 +6,6 @@ public class Main {
     static int[] dx = {1, -1, 0, 0};
     static boolean[][] visited;
     static boolean[][] fireVisited;
-
     static char[][] miro;
     static int[] startPos;
     static Queue<int[]> firePos = new LinkedList<>();
@@ -31,6 +30,7 @@ public class Main {
 
                 }else if (miro[i][j] == 'F') {
                     firePos.add(new int[]{i, j});
+                    fireVisited[i][j] = true;
 
                     // 벽은 방문표시로 갈 수 없게 만듦
                 }else if (miro[i][j] == '#') {
@@ -40,12 +40,10 @@ public class Main {
             }
         }
 
-
         int answer = bfs();
         bw.write(answer == - 1 ? "IMPOSSIBLE" : answer + "");
         bw.flush();
         bw.close();
-
     }
 
 
@@ -53,51 +51,51 @@ public class Main {
     // 불이 지훈 옆이라면 지훈 자리를 F
 
     private static int bfs() {
-        Queue<int[]> jihunPos = new LinkedList<>();
-        jihunPos.add(startPos);
+        Queue<int[]> jihoonPosQueue = new LinkedList<>();
         visited[startPos[0]][startPos[1]] = true;
+        jihoonPosQueue.add(startPos);
         int time = 0;
 
-        while (!jihunPos.isEmpty()) {
-            fireBfs(); // 불 퍼진 상태에서 지훈이 이동
-            int size = jihunPos.size();
+        while (!jihoonPosQueue.isEmpty()) {
+            fireBfs(); // 불 퍼뜨리기
+
+            int size = jihoonPosQueue.size();
             time++;
 
             // 1분 후 지훈이 이동 가능한 곳 이동
             while (size--> 0) {
-                int[] curPos = jihunPos.poll();
+                int[] curPos = jihoonPosQueue.poll();
 
-                if (curPos[0] == 0 || curPos[0] == miro.length - 1
-                        ||curPos[1] == 0 || curPos[1] == miro[0].length - 1) {
+                // 탈출 가능하면 걸린 시간 반환
+                if (escapeCheck(curPos)) {
                     return time;
                 }
-
 
                 for (int i = 0; i < 4; i++) {
                     int nextR = curPos[0] + dy[i];
                     int nextC = curPos[1] + dx[i];
 
+                    // 미로 범위를 넘어서는 지 확인, 방문했는지, 불인지 확인
                     if (boundaryCheck(nextR, nextC, miro)
                             || visited[nextR][nextC]
                             || miro[nextR][nextC] == 'F') continue;
 
-                    jihunPos.add(new int[]{nextR, nextC});
+                    jihoonPosQueue.add(new int[]{nextR, nextC});
                     visited[nextR][nextC] = true;
                 }
             }
-
         }
-
         return -1;
     }
 
     // 불 이동
+
     private static void fireBfs() {
         int cnt = firePos.size();
 
+        // 현재 불들을 전파
         while (cnt--> 0 && !firePos.isEmpty()) {
             int[] fire = firePos.poll();
-            fireVisited[fire[0]][fire[1]] = true;
 
             for (int i = 0; i < 4; i++) {
                 int newR = fire[0] + dy[i];
@@ -114,6 +112,13 @@ public class Main {
         }
     }
 
+    // 탈출 체크
+    private static boolean escapeCheck(int[] curPos) {
+        return curPos[0] == 0 || curPos[0] == miro.length - 1
+                || curPos[1] == 0 || curPos[1] == miro[0].length - 1;
+    }
+
+    // 미로 범위 체크
     private static boolean boundaryCheck(int newR, int newC, char[][] miro) {
         return newR < 0 || newR >= miro.length
                 || newC < 0 || newC >= miro[0].length;
