4f70514469ebcf7c4b92e849ab764cc9d3f252d0
CHANUK HEO <siai3647@gmail.com>
2025-03-19 23:12:32 +0900

    [level 3] Title: 정수 삼각형, Time: 15.99 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,29 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[500][500];
-        
-        // 왼쪽 위, 위
-        
+        int[][] dp = new int[triangle.length][triangle.length];
         dp[0][0] = triangle[0][0];
         
+        // [i-1] ([j -1], [j]) 중 제일 큰 값을 취함
         
         for (int i = 1; i < triangle.length; i++) {
-            for (int j = 0; j < triangle[i].length; j++) {
-                if (j - 1 >= 0 && triangle[i - 1].length > j){
-                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i-1][j]) + triangle[i][j];
-                } else if (j - 1 >= 0) {
-                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            for (int j = 0; j < i + 1; j++) {
+                if (j - 1 >= 0 && j <= i) {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i-1][j]);
+                } else if (j - 1 >= 0){
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (j <= i) {
+                    dp[i][j] = dp[i-1][j];
                 }
                 
+                dp[i][j] += triangle[i][j];
             }
         }
         
         
-        return Arrays.stream(dp[triangle.length - 1]).max().getAsInt();
+
+        return Arrays.stream(dp[dp.length - 1]).max().getAsInt();
     }
 }
