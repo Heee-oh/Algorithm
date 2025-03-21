c57a952b8fbccf7d351d760ed449d68bda13fc98
CHANUK HEO <siai3647@gmail.com>
2025-03-21 14:22:16 +0900

    [Silver I] Title: 스티커, Time: 648 ms, Memory: 114524 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -3,11 +3,10 @@ import java.util.*;
 
 public class Main {
 
-    // 1차원 배열로 가능한가 ?
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
@@ -30,10 +29,12 @@ public class Main {
                 }
             }
 
-            bw.write(Math.max(dp[0][n-1], dp[1][n-1]) + "\n");
+            sb.append(Math.max(dp[0][n - 1], dp[1][n - 1])).append("\n");
         }
 
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
+
 }
