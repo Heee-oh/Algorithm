365534c7da75ac52171fc12d9a367ffa5b219f68
CHANUK HEO <siai3647@gmail.com>
2025-05-05 09:47:44 +0900

    [Silver II] Title: 상자넣기, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -16,22 +16,24 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             boxes[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
+            dp[i] = 1; // 각 상자들은 현재 최대 1개의 상자를 넣을 수 있다 (즉 자기 자신)
         }
 
         int max = 0;
 
         for (int i = 1; i < n; i++) {
+            // i번째 이전 즉, 앞의 박스들과 비교하여 크기가 작은 상자들을 넣는다.
             for (int j = i - 1; j >= 0; j--) {
                 if (boxes[j] < boxes[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
 
+            // 이전에 구한 상자개수가 최댓값일 수 있으므로 max를 갱신
             max = Math.max(max, dp[i]);
         }
 
-        System.out.println(Math.max(max, dp[n-1]));
+        System.out.println(max);
     }
 
 }
