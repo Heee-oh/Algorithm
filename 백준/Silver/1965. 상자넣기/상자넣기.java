b8331e3ecbc60c60c65c54ce4b321706cb5815f6
CHANUK HEO <siai3647@gmail.com>
2025-05-05 09:55:55 +0900

    [Silver II] Title: 상자넣기, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -10,30 +10,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] boxes = new int[n];
-        int[] dp = new int[n];
+        int[] seq = new int[n];
+        int idx = 0;
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            boxes[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1; // 각 상자들은 현재 최대 1개의 상자를 넣을 수 있다 (즉 자기 자신)
-        }
+            int num = Integer.parseInt(st.nextToken());
 
-        int max = 0;
 
-        for (int i = 1; i < n; i++) {
-            // i번째 이전 즉, 앞의 박스들과 비교하여 크기가 작은 상자들을 넣는다.
-            for (int j = i - 1; j >= 0; j--) {
-                if (boxes[j] < boxes[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            if (idx == 0 || seq[idx - 1] < num) {
+                seq[idx++] = num;
+                continue;
+            }
+
+            int left = 0, right = idx - 1;
+
+            while (left < right) {
+
+                int mid = (left + right) >>> 1;
+
+                if (seq[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
                 }
             }
 
-            // 이전에 구한 상자개수가 최댓값일 수 있으므로 max를 갱신
-            max = Math.max(max, dp[i]);
+            seq[left] = num;
         }
 
-        System.out.println(max);
+        System.out.println(idx);
+
     }
 
 }
