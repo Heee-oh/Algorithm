6bc750e172c1de00118eb955a0c387a54053b95b
CHANUK HEO <siai3647@gmail.com>
2025-05-20 20:42:26 +0900

    [Gold III] Title: 정상 회담 2, Time: 156 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/1670. 정상 회담 2/정상 회담 2.java
+++ 백준/Gold/1670. 정상 회담 2/정상 회담 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Stack;
 
 public class Main {
     static final int MOD = 987654321;
@@ -15,8 +14,11 @@ public class Main {
         dp[4] = 2;
 
         for (int i = 6; i <= n; i += 2) {
-
+            // 1번 사람이 왼쪽사람과 악수하는 경우 (dp[i-2])
+            // 1번 사람이 오른쪽 사람과 악수하는 경우 (dp[i-2])
             dp[i] = (dp[i - 2] * 2) % MOD;
+
+            // 1번 사람이 나머지 팔이 교차하지 않는 위치에 있는 사람과 악수하는 경우의 수
             for (int j = i - 4; j >= 2; j -= 2) {
                 dp[i] = (dp[i] + dp[j] * dp[i - j - 2]) % MOD;
             }
@@ -24,9 +26,4 @@ public class Main {
 
         System.out.println(dp[n]);
     }
-
-    // 각각 하나씩 유전자를 빼보면서 최장길이를 찾음
-
-
-
 }
