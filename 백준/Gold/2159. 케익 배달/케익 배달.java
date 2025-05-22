01a9689feb521969a188aa67733acf0ba745333a
CHANUK HEO <siai3647@gmail.com>
2025-05-22 10:31:38 +0900

    [Gold III] Title: 케익 배달, Time: 696 ms, Memory: 64456 KB -BaekjoonHub

--- 백준/Gold/2159. 케익 배달/케익 배달.java
+++ 백준/Gold/2159. 케익 배달/케익 배달.java
@@ -6,6 +6,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int n;
+    static int X;
+    static int Y;
     static int[][] coordinates;
     static long[][] dp;
     static int[] dx = {0, 0, 1, 0, -1};
@@ -26,12 +28,15 @@ public class Main {
             coordinates[i][1] = y;
         }
 
+        X = coordinates[0][0];
+        Y = coordinates[0][1];
+
         for (int i = 0; i <= n; i++) {
             Arrays.fill(dp[i], -1);
         }
 
-        System.out.println(dfs(0, 0, new int[] {coordinates[0][0], coordinates[0][1]}));
-
+//        System.out.println(dfs(0, 0, new int[] {coordinates[0][0], coordinates[0][1]}));
+        System.out.println(dfs(0, 0));
     }
 
     // 0 : x, 1 : y
@@ -49,6 +54,33 @@ public class Main {
             int[] nextCur = new int[]{coordinates[idx][0] + dx[i], coordinates[idx][1] + dy[i]};
 
             dp[idx][type] = Math.min(dp[idx][type], movedDistance + dfs(idx + 1, i, nextCur));
+        }
+
+        return dp[idx][type];
+    }
+
+    // 0 : x, 1 : y
+    private static long dfs(int idx, int type) {
+        if (idx > n) return 0;
+        if (dp[idx][type] != -1) return dp[idx][type];
+
+        int pre_X, pre_Y;
+        if (idx == 0) {
+            pre_X = X;
+            pre_Y = Y;
+        } else {
+            pre_X = coordinates[idx - 1][0] + dx[type];
+            pre_Y = coordinates[idx - 1][1] + dy[type];
+        }
+
+        dp[idx][type] = Long.MAX_VALUE / 2;
+
+        for (int i = 0; i < 5; i++) {
+            int movedDistance = Math.abs(coordinates[idx][0] - pre_X+ dx[i])
+                    + Math.abs(coordinates[idx][1] - pre_Y+ dy[i]);
+
+            // 다음 위치를 생성
+            dp[idx][type] = Math.min(dp[idx][type], movedDistance + dfs(idx + 1, i));
         }
 
         return dp[idx][type];
