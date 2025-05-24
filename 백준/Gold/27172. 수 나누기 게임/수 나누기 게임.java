0db79f749deb098f7c8fb4ebf65a46beb7214c73
CHANUK HEO <siai3647@gmail.com>
2025-05-24 10:51:06 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 584 ms, Memory: 67096 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,64 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Main {
-    static boolean[] nums;
+    static final int MAX = 1000000;
     static int[] players;
-    static int[] sortedPlayers;
-    static int max;
-    static Map<Integer, Integer> idxMap = new HashMap<>();
     static int[] answer;
+    static Map<Integer, Integer> idxMap = new HashMap<>();
+    static int[] freq = new int[MAX + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         players = new int[n];
         answer = new int[n];
-        nums = new boolean[1000001];
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             players[i] = Integer.parseInt(st.nextToken());
-            idxMap.put(players[i], i); // 수에 대한 인덱스
-            nums[players[i]] = true; // 플레이어의 수 존재 표시
+            idxMap.put(players[i], i);
+            freq[players[i]]++;
         }
 
-        sortedPlayers = players.clone();
-        Arrays.sort(sortedPlayers);
-        max = sortedPlayers[n-1];
-
-        for (int i = 0; i < n; i++) {
-            nums[sortedPlayers[i]] = false;
-            sieveOfEratosthenes(i);
-        }
-
-
-        StringBuilder sb = new StringBuilder();
-        Arrays.stream(answer).forEach(x -> sb.append(x).append(" "));
-        System.out.println(sb.toString());
-
-    }
-
-    private static void sieveOfEratosthenes(int idx) {
-        int tmp = sortedPlayers[idx];
-        for (int i = 2; i <= max; i++) {
-            int target = tmp * i;
-            if (target > max) return;
-            
-            if (nums[target]) {
-                int curIdx = idxMap.get(tmp);
-                int targetIdx = idxMap.get(target);
-                answer[curIdx]++;
-                answer[targetIdx]--;
+        for (int i = 1; i <= MAX; i++) {
+            if (freq[i] == 0) continue;
+            for (int j = i * 2; j <= MAX; j += i) {
+                if (freq[j] > 0) {
+                    answer[idxMap.get(i)] += freq[j];
+                    answer[idxMap.get(j)] -= freq[i];
+                }
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int val : answer) sb.append(val).append(" ");
+        System.out.println(sb);
     }
-
-
 }
