03935719f9c863fa739cfa20e12bacd07ff9a9f5
CHANUK HEO <siai3647@gmail.com>
2025-06-08 20:38:30 +0900

    [Gold IV] Title: 호텔, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -16,38 +16,28 @@ public class Main {
         int[] customers = new int[N + 1];
 
         // 도시 , 1~C
-        int[][] dp = new int[N + 1][C + 101];
+        int[] dp = new int[C + 100];
+
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            costs[i] = Integer.parseInt(st.nextToken());
-            customers[i] = Integer.parseInt(st.nextToken());
-        }
+            int cost = Integer.parseInt(st.nextToken());
+            int customer = Integer.parseInt(st.nextToken());
 
-        Arrays.fill(dp[0], Integer.MAX_VALUE);
-
-        for (int c = 1; c <= C + 100; c++) {
-            for (int city = 1; city <= N; city++) {
-
-                // j홍보만 사용하여 c를 만족하는 경우
-                int cnt = (int) Math.ceil((double) c / customers[city]);
-                dp[city][c] = cnt * costs[city];
-
-                // j홍보 1개를 사용하면서 다른 최소값 홍보를 사용하는 경우
-                if (c >= customers[city]) {
-                    dp[city][c] = Math.min(dp[city][c], costs[city] + dp[city][c - customers[city]]);
-
-                }
-
-                dp[city][c] = Math.min(dp[city][c], dp[city-1][c]);
+            // customer ~ C+100 까지 (인덱스이므로 사실상 C+99)
+            for (int j = customer; j < C + 100; j++) {
+                dp[j] = Math.min(dp[j], cost + dp[j - customer]);
             }
-
         }
+
 
         int answer = Integer.MAX_VALUE;
-        for (int i = C; i <= C + 100; i++) {
-            answer = Math.min(answer, dp[N][i]);
+        for (int i = C; i < C + 100; i++) {
+            answer = Math.min(answer, dp[i]);
         }
+
         System.out.println(answer);
 
     }
