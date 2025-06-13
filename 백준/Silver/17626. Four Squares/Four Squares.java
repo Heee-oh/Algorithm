e61085b1dbc69e64e5d2acb3de0c1743d446a137
CHANUK HEO <siai3647@gmail.com>
2025-06-13 11:30:41 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -11,17 +11,16 @@ public class Main {
         dp[0] = 0;
         dp[1] = 1;
 
+        for (int i = 2; i * i <= n; i++) {
+            dp[i*i] = 1;
+        }
+
         for (int i = 2; i <= n; i++) {
-            int sqrt = (int) Math.sqrt(i);
+            if (dp[i] == 1) continue;
 
-            if (sqrt * sqrt == i) {
-                dp[i] = 1;
-
-            } else {
-                dp[i] = Integer.MAX_VALUE;
-                for (int j = 1; j <= Math.sqrt(i); j++) {
-                    dp[i] = Math.min(dp[i - (j * j)] + 1, dp[i]);
-                }
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                dp[i] = Math.min(dp[i - (j * j)] + 1, dp[i]);
             }
         }
 
