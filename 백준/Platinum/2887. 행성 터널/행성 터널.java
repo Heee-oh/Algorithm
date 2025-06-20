a89006cc37540e34fbeac45cb4bd98434d2c2646
CHANUK HEO <siai3647@gmail.com>
2025-06-20 10:43:54 +0900

    [Platinum V] Title: 행성 터널, Time: 1460 ms, Memory: 74540 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -5,10 +5,10 @@ import java.util.stream.IntStream;
 public class Main {
 
     static class Point implements Comparable<Point>{
-        long v;
+        int v;
         int idx;
 
-        public Point(long v, int idx) {
+        public Point(int v, int idx) {
             this.v = v;
             this.idx = idx;
         }
@@ -21,44 +21,38 @@ public class Main {
 
     static int[] parent;
 
-    static PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[0], o2[0]));
+    static PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o1[0], o2[0]));
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        parent = IntStream.range(0, n).toArray();
 
-        Point[] soredX = new Point[n];
-        Point[] soredY = new Point[n];
-        Point[] soredZ = new Point[n];
+        parent = IntStream.range(0, n).toArray();
+        Point[][] planets = new Point[3][n];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            long x = Long.parseLong(st.nextToken());
-            long y = Long.parseLong(st.nextToken());
-            long z = Long.parseLong(st.nextToken());
-            soredX[i] = new Point(x, i);
-            soredY[i] = new Point(y, i);
-            soredZ[i] = new Point(z, i);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            planets[0][i] = new Point(x, i);
+            planets[1][i] = new Point(y, i);
+            planets[2][i] = new Point(z, i);
 
         }
 
-        Arrays.sort(soredX); // x 기준 오름차순 정렬
-        Arrays.sort(soredY);  // y 기준 오름차순 정렬
-        Arrays.sort(soredZ);  // z 기준 오름찬순 정렬
+        for (int i = 0; i < 3; i++) {
+            Arrays.sort(planets[i]);
+            createEdge(n, planets[i]);
+        }
 
         long answer = 0;
         int cnt = 0;
-
-        createEdge(n, soredX);
-        createEdge(n, soredY);
-        createEdge(n, soredZ);
-
         while (!pq.isEmpty()) {
             if (cnt == n-1) break;
 
-            long[] info = pq.poll();
-            int v1 = (int) info[1];
-            int v2 = (int) info[2];
+            int[] info = pq.poll();
+            int v1 = info[1];
+            int v2 = info[2];
 
             if (find(v1) != find(v2)) {
                 union(v1,v2);
@@ -93,7 +87,7 @@ public class Main {
             Point p1 = point[i];
             Point p2 = point[i - 1];
 
-            pq.add(new long[]{Math.abs(p1.v - p2.v), p1.idx, p2.idx});
+            pq.add(new int[]{Math.abs(p1.v - p2.v), p1.idx, p2.idx});
 
         }
     }
