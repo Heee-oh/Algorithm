9e7cf7abac9b89dcb7f0be3aa5734f23efd76647
CHANUK HEO <siai3647@gmail.com>
2025-06-22 17:11:31 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 800 ms, Memory: 185540 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -9,7 +9,9 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
         int[] dp = new int[N];
-        int[] prev = new int[N];
+        int[] prev = new int[N]; // dp에서 이전 증가하는 수열의 값의 실제 arr 내 idx 저장
+        int[] dpIdx = new int[N]; // DP에 저장된 실제 수열 arr에서의 idx 를 저장 
+
         Arrays.fill(prev, -1);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -23,11 +25,13 @@ public class Main {
 
             if (dp[size - 1] < arr[i]) {
                 dp[size] = arr[i];
-                prev[i] = size - 1;
+                dpIdx[size] = i;
+                prev[i] = dpIdx[size - 1];
                 size++;
                 continue;
             }
 
+            // 이분 탐색으로 증가하는 부분수열에서 적절한 위치를 탐색
             int left = 0, right = size - 1;
             while (left < right) {
 
@@ -41,21 +45,25 @@ public class Main {
             }
 
             dp[left] = arr[i];
+            dpIdx[left] = i;
             if (left > 0) {
-                prev[i] = left - 1;
+                prev[i] = dpIdx[left - 1];
             }
         }
+
 
         Stack<Integer> stack = new Stack<>();
         sb.append(size).append("\n");
-        int target = size - 2;
 
-        for (int i = N - 1; i >= 0 ; i--) {
-            if (target == prev[i]) {
-                stack.push(arr[i]);
-                target--;
-            }
+
+        int idx = dpIdx[size-1];
+
+        while (size > 0) {
+            stack.push(arr[idx]);
+            idx = prev[idx];
+            size--;
         }
+
 
         while (!stack.isEmpty()) {
             sb.append(stack.pop()).append(" ");
