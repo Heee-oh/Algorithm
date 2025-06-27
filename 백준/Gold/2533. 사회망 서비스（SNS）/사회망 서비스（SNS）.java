d8828175c29e4d8c6f42d26876ee3389d03dfd5e
CHANUK HEO <siai3647@gmail.com>
2025-06-27 11:04:58 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2212 ms, Memory: 427232 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -15,7 +15,6 @@ public class Main {
         dp = new int[n + 1][2];
         for (int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
-            Arrays.fill(dp[i], -1);
         }
 
         for (int i = 1; i < n; i++) {
@@ -26,40 +25,27 @@ public class Main {
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
-        graph[0].add(1);
-        System.out.println(dfs(0, 1));
+        visited[1] = true;
+        dfs(1);
+
+        System.out.println(Math.min(dp[1][0], dp[1][1]));
 
     }
 
 
     // 켜져있을 시 , 자식노드는 키거나 끌 수 있음
     // 꺼져있을 시 , 자식노드는 무조건 켜야함
-    private static int dfs(int node, int onOff) {
-        if (graph[node].isEmpty()) {
-            return 0;
-        }
-
-        if (dp[node][onOff] != -1) {
-            return dp[node][onOff];
-        }
-
-        dp[node][onOff] = 0;
+    private static void dfs(int node) {
+        dp[node][1] = 1;
         for (int next : graph[node]) {
             if (!visited[next]) {
                 visited[next] = true;
+                dfs(next);
 
-                // 부모가 켜져있을 경우
-                if (onOff == 1) {
-                    dp[node][onOff] += Math.min(dfs(next, 0), 1 + dfs(next, 1));
+                dp[node][0] += dp[next][1];
+                dp[node][1] += Math.min(dp[next][0], dp[next][1]);
 
-                    // 부모가 꺼졌을 경우
-                } else {
-                    dp[node][onOff] +=  1 + dfs(next, 1);
-                }
-                visited[next] = false;
             }
         }
-
-        return dp[node][onOff];
     }
 }
