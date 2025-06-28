a854c5d46e467e47d9dac6af00cfef162a91739e
CHANUK HEO <siai3647@gmail.com>
2025-06-28 09:04:27 +0900

    [Gold III] Title: 개미굴, Time: 144 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -2,49 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int MAX_LEVEL = 15;
+
+    static class Node {
+        Map<String, Node> map;
+
+        public Node() {
+            this.map = new TreeMap<>();
+        }
+
+        public Node addChild(String child) {
+            Node childNode = new Node();
+            map.putIfAbsent(child, childNode); // 존재하면 존해하는 키에 대한 value를 반환, 아니라면 새로 생성후 반환
+            return map.get(child);
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        String[] input = new String[n];
-        Set<String>[] levels = new Set[MAX_LEVEL];
-
-        for (int i = 0; i < MAX_LEVEL; i++) {
-            levels[i] = new HashSet<>();
-        }
+        Node root = new Node();
 
         for (int i = 0; i < n; i++) {
-            input[i] = br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cnt = Integer.parseInt(st.nextToken());
 
-            if (Character.isDigit(input[i].charAt(1))) {
-                input[i] = input[i].substring(3);
-            } else {
-                input[i] = input[i].substring(2);
-            }
-
-        }
-
-        Arrays.sort(input);
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < n; i++) {
-            StringBuilder check = new StringBuilder();
-            int idx = 0;
-            for (String str : input[i].split(" ")) {
-                check.append(str);
-
-                if (!levels[idx].contains(check.toString())) {
-                    levels[idx].add(check.toString());
-                    sb.append("--".repeat(idx)).append(str);
-                    sb.append("\n");
-                }
-                idx++;
+            Node currentNode = root;
+            for (int j = 0; j < cnt; j++) {
+                String feed = st.nextToken();
+                // 다음 노드로 이동
+                currentNode = currentNode.addChild(feed);
             }
         }
 
-        System.out.print(sb.toString());
+        dfs(root, 0);
+        System.out.println(sb.toString());
+
     }
+
+    static StringBuilder sb = new StringBuilder();
+    private static void dfs(Node root, int level) {
+        if (root.map.isEmpty()) {
+            return;
+        }
+
+        for (String child : root.map.keySet()) {
+            Node current = root.map.get(child);
+            sb.append(("--").repeat(level)).append(child).append("\n");
+            dfs(current, level + 1);
+        }
+    }
+
 }
