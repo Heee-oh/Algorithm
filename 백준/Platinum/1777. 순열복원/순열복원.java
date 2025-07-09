2a97b9d952ceaf0eee42f51d55f4c4177207e1c0
CHANUK HEO <siai3647@gmail.com>
2025-07-09 09:28:41 +0900

    [Platinum V] Title: 순열복원, Time: 384 ms, Memory: 30220 KB -BaekjoonHub

--- 백준/Platinum/1777. 순열복원/순열복원.java
+++ 백준/Platinum/1777. 순열복원/순열복원.java
@@ -2,50 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new LinkedList<>();
+    static int n;
+    static int[] tree, arr, ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        ans = new int[n + 1];
+
+        int h = (int) Math.ceil(Math.log(n) / Math.log(2));
+        tree = new int[1 << (h + 1)];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            if (num == 0) {
-                list.add(i);
-                continue;
-            }
-
-            int left = 0, right = list.size() - 1;
-            while (left <= right) {
-
-                int mid = (left + right) >>> 1;
-
-                if (list.size() - mid < num) {
-                    right = mid - 1;
-                }  else if (list.size() - mid > num){
-                    left = mid + 1;
-                } else {
-                    left = mid;
-                    break;
-                }
-            }
-
-            list.add(left, i);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int number : list) {
-            sb.append(number).append(" ");
+        init(1, 1, n);
+
+
+        for (int i = n; i > 0; i--) {
+            int idx = query(1, 1, n, arr[i]);
+            ans[idx] = i;
+        }
+
+        for (int i = n; i > 0; i--) {
+            sb.append(ans[i]).append(" ");
         }
 
         System.out.println(sb.toString().trim());
-
-
     }
 
+    private static int init(int treeNode, int s, int e) {
+        if (s == e) {
+            return tree[treeNode] = 1;
+        }
 
+        int mid = (s + e) >>> 1;
+
+        return tree[treeNode] = init(treeNode * 2, s, mid) + init(treeNode * 2 + 1, mid + 1, e);
+    }
+
+    private static int query(int treeNode, int s, int e, int cnt) {
+        tree[treeNode]--;
+
+        if (s == e) {
+            return s;
+        }
+        int mid = (s + e) >>> 1;
+
+
+        return cnt < tree[treeNode * 2] ?
+                query(treeNode * 2, s, mid, cnt) :
+                query(treeNode * 2 + 1, mid + 1, e, cnt - tree[treeNode * 2]);
+    }
 
 
 
