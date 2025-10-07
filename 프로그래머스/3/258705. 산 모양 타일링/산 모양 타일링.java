df6e2f4c32508b3c49fc939a62ccc8eabf04e9fe
CHANUK HEO <siai3647@gmail.com>
2025-10-07 10:54:46 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.87 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,33 +1,33 @@
 class Solution {
-    
-    static int MOD = 10_007;
+    static final int MOD = 10_007;
     public int solution(int n, int[] tops) {
-        int[] sharedDp = new int[n];
-        int[] noSharedDp = new int[n];
+        int answer = 0;
+        
+        int[] share = new int[n];
+        int[] notShare = new int[n];
         
         if (tops[0] == 1) {
-            sharedDp[0] = 4;
-            noSharedDp[0] = 3;
+            share[0] = 4;
+            notShare[0] = 3;
         } else {
-            sharedDp[0] = 3;
-            noSharedDp[0] = 2;
+            share[0] = 3;
+            notShare[0] = 2;
         }
+        
         
         for (int i = 1; i < n; i++) {
             if (tops[i] == 1) {
-                
-                sharedDp[i] = (sharedDp[i-1] * 3 + noSharedDp[i-1]) % MOD;
-                noSharedDp[i] = (sharedDp[i-1] * 2 + noSharedDp[i-1]) % MOD;
-                
+                share[i] = (share[i-1] * 3 + notShare[i-1]) % MOD;
+                notShare[i] = (share[i-1] * 2 + notShare[i-1]) % MOD;
             } else {
-                                
-                sharedDp[i] = (sharedDp[i-1] * 2 + noSharedDp[i-1]) % MOD;
-                noSharedDp[i] = (sharedDp[i-1] + noSharedDp[i-1]) % MOD;
+                share[i] = (share[i-1] * 2 + notShare[i-1]) % MOD;
+                notShare[i] = (share[i-1] + notShare[i-1]) % MOD;
             }
-            
-            
         }
         
-        return sharedDp[n-1];
+        
+        
+        
+        return share[n-1];
     }
 }
