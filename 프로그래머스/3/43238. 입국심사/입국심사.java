8a48d255d623bd680bbd7b8b66d4f2b8e009e54b
CHANUK HEO <siai3647@gmail.com>
2025-10-20 16:20:04 +0900

    [level 3] Title: 입국심사, Time: 100.12 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        
+        long left = 1; // 최소 시간
+        long right = (long) times[times.length - 1] * n;
+        long answer = right;
+
+        while (left <= right) {
+            long mid = (left + right) >>> 1;
+            long count = 0;
+
+            for (int time : times) {
+                count += mid / time; 
+                if (count >= n) break; 
+            }
+
+            if (count >= n) {
+                answer = mid; 
+                right = mid - 1;
+            } else {
+                left = mid + 1; 
+            }
+        }
+
+        return answer;
+    }
+}
