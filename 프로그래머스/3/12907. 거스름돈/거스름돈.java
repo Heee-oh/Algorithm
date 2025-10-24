ac779df74126f5fdd2c4ed95d18818f35e385179
CHANUK HEO <siai3647@gmail.com>
2025-10-24 14:02:34 +0900

    [level 3] Title: 거스름돈, Time: 43.98 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    static final int MOD = 1_000_000_007;
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        
+        Arrays.sort(money);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        
+        for (int i = 0; i < money.length; i++) {
+            for (int j = money[i]; j <= n; j++) {
+                dp[j] = (dp[j] + dp[j - money[i]]) % MOD;
+            }
+        }
+        
+        
+        return dp[n];
+    }
+}
