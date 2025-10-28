de8df4c885b8d4aa33448c497d03d76650e2bb29
CHANUK HEO <siai3647@gmail.com>
2025-10-28 10:26:56 +0900

    [level 3] Title: 순위, Time: 15.56 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,71 +1,36 @@
-import java.util.*;
-
 class Solution {
-    private static int cnt;
-    private static List<Integer>[] winGraph;
-    private static List<Integer>[] defeatGraph;
-    
     public int solution(int n, int[][] results) {
         int answer = 0;
-        
-        // 이긴 그래프, 지는 그래프 생성
-        winGraph = new List[n+1];
-        defeatGraph = new List[n+1];
-        
-        for (int i = 0; i <= n; i++) {
-            winGraph[i] = new ArrayList<>();
-            defeatGraph[i] = new ArrayList<>();
+
+        boolean[][] chk = new boolean[n + 1][n + 1];
+
+        for(int i = 0; i < results.length; i++) {
+            chk[results[i][0]][results[i][1]] = true;
         }
-        
-        
-        for (int[] res : results) {
-            int win = res[0];
-            int defeat = res[1];
-            
-            // 나에게 진사람을 리스트에 저장
-            defeatGraph[win].add(defeat);
-            // 현재 나를 이긴 사람을 리스트에 저장 
-            winGraph[defeat].add(win); 
+
+        for(int k = 1; k < n + 1; k++) {
+            for(int i = 1; i < n + 1; i++) {
+                for(int j = 1; j < n + 1; j++) {
+                    if(i != j && chk[i][k] && chk[k][j]) {
+                        chk[i][j] = true;
+                    }
+                }
+            }
         }
-        
-        for (int i = 1; i <= n; i++) {
-            cnt = 0;
-            boolean[] visited = new boolean[n+1];
-            upDfs(i, visited);
-            downDfs(i, visited);
-            if (cnt == n - 1 ) {
+
+        for(int i = 1; i < n + 1; i++) {
+            boolean pass = true;
+            for(int j = 1; j < n + 1; j++) {
+                if(i != j && !(chk[i][j] || chk[j][i])) {
+                    pass = false;
+                    break;
+                }
+            }
+            if(pass) {
                 answer++;
             }
         }
-        
+
         return answer;
     }
-    
-    private void upDfs(int cur, boolean[] visited) {
-       
-        // 상위 탐색 (현재 나를 이긴 위의 랭커들 탐색)
-        for (int i = 0; i < winGraph[cur].size(); i++) {
-            int next = winGraph[cur].get(i);
-            
-            if (!visited[next]) {
-                visited[next] = true;
-                cnt++;
-                upDfs(next, visited);
-            }
-        }
-    }
-    
-    private void downDfs(int cur, boolean[] visited) {
-        for (int i = 0; i < defeatGraph[cur].size(); i++) {
-            int next = defeatGraph[cur].get(i);
-            
-            if (!visited[next]) {
-                visited[next] = true;
-                cnt++;
-                downDfs(next, visited);
-            }
-        }
-    }
-    
-    
 }
