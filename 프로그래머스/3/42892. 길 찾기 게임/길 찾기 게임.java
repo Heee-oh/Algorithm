50bdc5f3c6785da3d206295152c93665a337d7c9
CHANUK HEO <siai3647@gmail.com>
2025-10-29 11:46:54 +0900

    [level 3] Title: 길 찾기 게임, Time: 213.52 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -5,7 +5,6 @@ class Solution {
     
     class Node {
         int n, x, y;
-        
         Node left;
         Node right;
         
@@ -24,9 +23,11 @@ class Solution {
     
     static int preOrderIdx = 0;
     static int postOrderIdx = 0;
+    
     public int[][] solution(int[][] nodeinfo) {
         answer = new int[2][nodeinfo.length];
         
+        // 노드 객체로 생성후 리스트에 담기
         for (int i = 0; i < nodeinfo.length; i++) {
             int x = nodeinfo[i][0];
             int y = nodeinfo[i][1];
@@ -34,12 +35,14 @@ class Solution {
         }
         
         visited = new boolean[nodeinfo.length + 1];
-        Collections.sort(list, (a, b) -> b.y == a.y ? a.x - b.x : b.y - a.y);
+        Collections.sort(list, (a, b) -> b.y == a.y ? a.x - b.x : b.y - a.y); // y가 큰순으로, 같다면 x가 작은 순으로 
         
-        Node head = list.get(0);
+        Node head = list.get(0); // 이진 트리이므로 항상 루트가 있을 것이다. 
         visited[head.n] = true;
-        buildBinaryTree(head, 1, 0, 100000);
         
+        buildBinaryTree(head, 1, 0, 100000); // 이진트리의 자식 노드를 설정할 수 있는 범위를 정해 올바른 이진트리생성
+        
+        // 전위, 후위 노드 탐색 
         preOrder(head);
         postOrder(head);
         
@@ -51,26 +54,33 @@ class Solution {
             return;
         }
         answer[0][preOrderIdx++] = cur.n;
-        preOrder(cur.left);
-        preOrder(cur.right);
+        if (cur.left != null) {
+            preOrder(cur.left);
+        }
+        if (cur.right != null) {
+            preOrder(cur.right);
+        }
     }
     
     
     private void postOrder(Node cur) {
-        if (cur == null) {
-            return;
+        if (cur.left != null) {
+            postOrder(cur.left);
         }
-        
-        postOrder(cur.left);
-        postOrder(cur.right);
+        if (cur.right != null) {
+            postOrder(cur.right);
+        }
         answer[1][postOrderIdx++] = cur.n;
     }
+    
     private void buildBinaryTree(Node cur, int idx, int l, int r) {
         
         for (int i = idx; i < list.size(); i++) {
             Node tmp = list.get(i);
-            if (visited[tmp.n] || cur.y == tmp.y) continue;
-            if (tmp.x < l || tmp.x > r) continue;
+            if (visited[tmp.n] // 이미 사용한 노드
+                || cur.y == tmp.y  // 같은 level의 노드 
+                || tmp.x < l || tmp.x > r) continue; // 자식노드 추가 가능한 범위를 벗어남
+         
             
             if (cur.left == null && tmp.x < cur.x) {
                 cur.left = tmp;
@@ -83,14 +93,11 @@ class Solution {
                 buildBinaryTree(tmp, i + 1, cur.x, r);
             }
             
-            
             if (cur.left != null && cur.right != null) {
                 break;
             }
             
         }
-        
-        
         
     }
 }
