c47eee158a25e93de3435afe924fdf8cd11b8a37
CHANUK HEO <siai3647@gmail.com>
2025-11-11 09:58:25 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,10 +1,13 @@
 -- 보호소, 중성화 수술 거친 동물 정보 조회
 -- 들어올 당시 중성화 X, 나갈 때 중성화된애들
 -- ID, 생물 종, 이름, ID 순 조회
-SELECT AI.ANIMAL_ID, AI.ANIMAL_TYPE, AI.NAME
-FROM ANIMAL_INS AI 
-INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE AI.SEX_UPON_INTAKE LIKE 'Intact%'
-AND (AO.SEX_UPON_OUTCOME LIKE 'Spayed%'
+SELECT AO.ANIMAL_ID, AO.ANIMAL_TYPE, AO.NAME
+FROM ANIMAL_OUTS AO 
+WHERE EXISTS (SELECT 1
+                           FROM ANIMAL_INS AI
+                           WHERE AI.ANIMAL_ID = AO.ANIMAL_ID
+                           AND AI.SEX_UPON_INTAKE LIKE 'Intact%'
+                          )
+AND (AO.SEX_UPON_OUTCOME LIKE 'Spayed%' 
 OR AO.SEX_UPON_OUTCOME LIKE 'Neutered%')
-ORDER BY AI.ANIMAL_ID ASC;
+ORDER BY AO.ANIMAL_ID ASC;
