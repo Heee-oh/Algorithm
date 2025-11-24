8a1752ed78378e17f4fbbe01b7f43091332fe7cd
CHANUK HEO <siai3647@gmail.com>
2025-11-24 10:17:40 +0900

    [level 3] Title: 억억단을 외우자, Time: 323.30 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -4,14 +4,17 @@ class Solution {
         int[] arr = new int[e + 1];
         int[] maxIdx = new int[e + 1];
         
-        
+        // 에라토스테네스의 체 활용
+        // 소수가 아닌 배수들의 개수 카운트 (이는 즉 약수의 개수가 됨)
         for (int i = 1; i <= e; i++) {
             for (int j = i; j <= e; j += i) {
                 arr[j] += 1;
             }
         }
         
-        maxIdx[e] = e;
+        maxIdx[e] = e; // e~e에서 가장 많이 등장하는 것은 e
+        
+        // i~e 구간에서 가장 많이 등장하면서 작은 수를 기록
         for (int i = e-1; i >= 1; i--) {
             // 현재 i번째 약수 개수가 이전 i+1의 최대 약수 개수를 가지는 idx의 약수개수보다 많거나 같다면
             if (arr[i] >= arr[maxIdx[i+1]]) {
@@ -23,6 +26,8 @@ class Solution {
             
         }
         
+        // starts의 각 원소로 s~e구간에서 정답을 maxIdx에 기록하였으므로 
+        // idx로 접근
         for (int i = 0; i < starts.length; i++) {
             int n = starts[i];
             answer[i] = maxIdx[n];
