40b3681dd12792c6e9b3e7b1d633cd9a625b7e9e
CHANUK HEO <siai3647@gmail.com>
2025-12-28 18:51:45 +0900

    [Gold V] Title: 창영이와 커피, Time: 132 ms, Memory: 15420 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
 class Main {
 
     public static void main(String[] args) throws IOException {
@@ -17,16 +16,8 @@ class Main {
         dp[0] = 0;
 
         st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
         for (int i = N - 1; i >= 0; i--) {
-            int C = arr[i];
+            int C =  Integer.parseInt(st.nextToken());
 
             for (int j = K; j >= C; j--) {
                 if (dp[j - C] != -1) {
