000403d42cf81b45416e4537511becd56ee1818e
CHANUK HEO <siai3647@gmail.com>
2026-01-09 16:39:48 +0900

    [level 2] Title: 배달, Time: 5.24 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,61 +1,66 @@
 import java.util.*;
 
 class Solution {
-    // N개의 마을, K : 갈 수 있는 거리
-    // BFS 탐색으로 K - 각 마을까지의 비용 >= 0 인 곳을 방문
-    int[] visited;
+    
+    int[] dist;
+    boolean[] visited;
     
     public int solution(int N, int[][] road, int K) {
-        int[][] graph = new int[N + 1][N + 1]; 
-        visited = new int[N + 1];
+        int answer = 0;
+        List<int[]>[] graph = new ArrayList[N+1];
+        dist = new int[N+1];
+        visited = new boolean[N+1];
         
-        // 마을 그래프 생성
-        for (int i = 0; i < road.length; i++) {
-            int start = road[i][0];
-            int end = road[i][1];
-            
-            
-            // 양방향, 걸리는 시간 최소값
-            graph[start][end] = graph[end][start] 
-                = ((graph[start][end] > 0) ? 
-                   Math.min(graph[start][end], road[i][2]) : road[i][2]);
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
-
-        return bfs(graph, K);
+        
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        for (int[] info : road) {
+            int a = info[0];
+            int b = info[1];
+            int c = info[2];
+            
+            graph[a].add(new int[] {b,c});
+            graph[b].add(new int[] {a,c});
+            
+        }
+        dijkstra(graph);
+        
+        for (int i = 1; i <= N; i++) {
+            answer += dist[i] <= K ? 1 : 0;
+        }
+        return answer;
     }
     
-    private int bfs(int[][] towns, int k) {
-        Queue<int[]> q = new LinkedList<>(); // [다음 마을, 남은 k]
-        q.add(new int[] {1, 0}); // 문제에서 '1번 마을에 있는 음식점'
-        visited[1] = k;
-        int count = 1; // 1번 마을도 포함
+    private void dijkstra(List<int[]>[] graph) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.add(new int[] {1, 0});
+        dist[1] = 0;
         
-        while(!q.isEmpty()) {
-            int[] curTownInfo = q.poll();
-            int idx = curTownInfo[0];
-
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int n = cur[0];
+            int cost = cur[1];
             
-            // 0이면 더이상 배달할 시간이 없음
-            if (curTownInfo[1] == k) continue;
-                     
-            // 각 연결된 마을들을 순회
-            for (int i = 1; i < towns.length; i++) {
-                if (towns[idx][i] > 0 
-                    && (curTownInfo[1] + towns[idx][i]) <= k) {// 남은 배달시간 안에 가능하다면 
-                    
-                    // 같은 마을을 가는데 더 비용이 적게 든다면
-                    if (visited[i] > curTownInfo[1] + towns[idx][i]) {
-                        q.add(new int[] {i, (curTownInfo[1] + towns[idx][i])});
-                        visited[i] = curTownInfo[1] + towns[idx][i];
-                        
-                    } else if (visited[i] == 0) {
-                        q.add(new int[] {i, (curTownInfo[1] + towns[idx][i])});
-                        visited[i] = curTownInfo[1] + towns[idx][i];
-                        count++;
-                    }
+            if (visited[n]) {
+                continue;
+            }
+            
+            for (int i = 0; i < graph[n].size(); i++) {
+                int[] next = graph[n].get(i);
+                int nextN = next[0];
+                int nextCost = next[1];
+                
+                if (dist[nextN] > dist[n] + nextCost) {
+                    dist[nextN] = dist[n] + nextCost;
+                    pq.add(new int[] {nextN, dist[nextN]});
                 }
             }
+            
+            
         }
-        return count;
+        
     }
 }
