774d2e1c94ec59a842919ebe157997d9b8e9a035
CHANUK HEO <siai3647@gmail.com>
2026-03-04 18:28:14 +0900

    [Gold II] Title: 교환, Time: 176 ms, Memory: 49884 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.java
+++ 백준/Gold/1039. 교환/교환.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 
     static int N, K;
-    static boolean[][] visited = new boolean[1000001][11];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,54 +12,66 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(N);
-        visited[N][0] = true;
+        int len = String.valueOf(N).length();
+        boolean[][] visited = new boolean[(int) (1e6 + 1)][K + 1];
+        int max = - 1;
 
-        int answer = -1;
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(N);
 
-        for (int depth = 0; depth < K; depth++) {
+        // k 번 바꿀 경우
+        for (int i = 1; i <= K; i++) {
             int size = q.size();
 
-            if (size == 0) break;
-
-            for (int s = 0; s < size; s++) {
+            while (size-- > 0) {
                 int cur = q.poll();
-                char[] arr = String.valueOf(cur).toCharArray();
 
-                int len = arr.length;
+                char[] num = String.valueOf(cur).toCharArray();
 
-                for (int i = 0; i < len - 1; i++) {
-                    for (int j = i + 1; j < len; j++) {
+                // 각 자리를 바꾸는 모든 경우의 수 탐색
+                for (int j = 0; j < len-1; j++) {
 
-                        // swap
-                        char temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
+                    for (int k = j + 1; k < len; k++) {
 
-                        // 맨 앞이 0이면 skip
-                        if (arr[0] != '0') {
-                            int next = Integer.parseInt(new String(arr));
+                        char tmp = num[j];
+                        num[j] = num[k];
+                        num[k] = tmp;
 
-                            if (!visited[next][depth + 1]) {
-                                visited[next][depth + 1] = true;
-                                q.offer(next);
+                        // 0으로 시작하지 않으면 가능한 수
+                        if (num[0] != '0') {
+                            int res = 0;
+                            for (int l = 0; l < len; l++) {
+                                res = res * 10 + (num[l] - '0');
+                            }
+
+                            // 중복 방지
+                            if (visited[res][i]) {
+                                num[k] = num[j];
+                                num[j] = tmp;
+                                continue;
+                            }
+                            visited[res][i] = true;
+                            q.offer(res);
+
+                            // 마지막 K 번째 체인지일때 최대값 갱신
+                            if (i == K) {
+                                max = Math.max(max, res);
                             }
                         }
 
-                        // 원상복구
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
+                        num[k] = num[j];
+                        num[j] = tmp;
                     }
                 }
+
+
             }
+
+
         }
 
-        while (!q.isEmpty()) {
-            answer = Math.max(answer, q.poll());
-        }
 
-        System.out.println(answer);
+        System.out.println(max);
+
     }
 }
