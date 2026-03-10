a5a20a75ad8c58af7a11d4e59a2300c68ef5f611
CHANUK HEO <siai3647@gmail.com>
2026-03-10 18:27:47 +0900

    [Gold III] Title: 구간 나누기, Time: 116 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/2228. 구간 나누기/구간 나누기.java
+++ 백준/Gold/2228. 구간 나누기/구간 나누기.java
@@ -1,32 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] dp;
-    static int[] sum;
 
-    static int N;
-    static int M;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        int N = fr.nextInt();
+        int M = fr.nextInt();
 
-        dp = new int[N + 1][M + 1];
-        sum = new int[N + 1];
+        int[] sum = new int[N + 1];
 
-        // 누적합을 구한다.
         for (int i = 1; i <= N; i++) {
-            sum[i] = sum[i - 1] + Integer.parseInt(br.readLine());
+            sum[i] = sum[i - 1] + fr.nextInt();
         }
 
+        int[][] dp = new int[N + 1][M + 1];
         for (int i = 0; i <= N; i++) {
-            // 1~M 까지 구간에 대해여 초기값을 -무한 으로 변경
             for (int j = 1; j <= M; j++) {
                 dp[i][j] = Integer.MIN_VALUE / 2;
             }
@@ -34,33 +25,66 @@ public class Main {
 
         dp[1][1] = sum[1];
 
-        // n개를 m개의 구간으로 나누기
         for (int n = 2; n <= N; n++) {
             for (int m = 1; m <= M; m++) {
 
-                // case 1 n을 현재 구간에 미포함 하는 경우
+                // n을 현재 구간에서 미포함하는 경우 이전 최댓값을 이어받음
                 dp[n][m] = dp[n - 1][m];
 
-                // m = 1일때 첫번째 구간을 얻어야한다. 따라서 k를 1~N까지 탐색하게 한다.
-                int min = (m == 1) ? -1 : 0;
+                // m == 1일때 첫번째 구간 구하기
+                int min = m == 1 ? -1 : 0;
 
-                // case 2 n을 현재 구간에 포함하는 경우 한칸 띄고 포함해야한다. (...n-2) n-1 (n) 이런식으로 때문에 k = n-2 부터
-                for (int k = n - 2; k >= min; k--) {
-
-                    if (k < 0) { // k가 -라는 것은 앞에 아무것도 없다는 의미 즉, 구간이 존재하지 않기에 1~n까지의 구간을 생성허요 첫번째 구간으로 잡는다.
-                        dp[n][m] = Math.max(dp[n][m],sum[n]);
-
+                // n번째 숫자를 포함하는 경우 한칸 띄기
+                for (int k = n-2; k >= min; k--) {
+                    if (k < 0) {
+                        dp[n][m] = Math.max(dp[n][m], sum[n]);
                     } else {
-                        dp[n][m] = Math.max(dp[n][m], dp[k][m - 1] + sum[n] - sum[k + 1]);
+                        //  k + 1을 건너띔
+                        // (1..k)최댓값 [k+1] (k+2 ... n)
+                        int tmp = sum[n] - sum[k + 1];
+                        dp[n][m] = Math.max(dp[n][m], dp[k][m - 1] + tmp);
                     }
-
                 }
-
-
             }
         }
 
+
         System.out.println(dp[N][M]);
+
     }
 
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
 }
