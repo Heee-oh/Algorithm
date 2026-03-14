a828ca1f1a604680befef32cba5bd87be05ee3f5
CHANUK HEO <siai3647@gmail.com>
2026-03-14 18:04:57 +0900

    [Gold II] Title: N포커, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/16565. N포커/N포커.java
+++ 백준/Gold/16565. N포커/N포커.java
@@ -1,43 +1,39 @@
+
 import java.io.*;
-import java.sql.SQLOutput;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+    static final int MOD = 10_007;
 
-    static final long MOD = 10_007;
-    // 계산은 long
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        int N = fr.nextInt();
 
-        // 4카드 이하면 이길 수 없음
-        if (n < 4) {
+        if (N < 4) {
             System.out.println(0);
             return;
         }
 
         int[][] dp = new int[53][53];
 
-        dp[0][0] = 1;
-        for (int i = 1; i < 53; i++) {
-            dp[i][1] = i;
-            dp[i][0] = dp[i][i] = 1;
-            for (int j = 2; j < i; j++) {
-                dp[i][j] = (int) ((dp[i - 1][j] + dp[i - 1][j - 1]) % MOD);
+        for (int i = 0; i <= 52; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= 52; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
             }
         }
 
         long ans = 0;
-        int max = n / 4;  // 가능한 최대 포카드 쌍 개수
+        for (int k = 1; k <= N / 4; k++) {
+            long rest = dp[52 - 4 * k][N - 4 * k];
+            long pickForCard = dp[13][k];
 
-        for (int i = 1; i <= max; i++) {
-            // i쌍의 포카드를 뽑는 경우의 수
-            long pickFourCard = dp[13][i];                   // C(13, i)
-            long rest  = dp[52 - 4*i][n - 4*i];              // C(52 - 4*i, N - 4*i)
-            long cnt = (pickFourCard * rest) % MOD;
-
-            // 뽑은 포카드의 쌍의 수가 홀수일때는 더하고, 짝수일떄는 뺀다. 
-            if (i % 2 == 1) {
+            long cnt = (pickForCard * rest) % MOD;
+            if ((k & 1) == 1) {
                 ans = (ans + cnt) % MOD;
             } else {
                 ans = (ans - cnt + MOD) % MOD;
@@ -46,6 +42,42 @@ public class Main {
 
         System.out.println(ans);
 
+
     }
 
+
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
 }
