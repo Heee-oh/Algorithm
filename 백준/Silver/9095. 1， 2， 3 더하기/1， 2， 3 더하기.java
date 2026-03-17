a97c2eae5ab996b6642ffaaab32f1e49e51f90bf
CHANUK HEO <siai3647@gmail.com>
2026-03-17 12:25:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,35 +1,69 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
+        FastReader fr = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        int T = fr.nextInt();
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        int[] DP = new int[12];
-        DP[1] = 1;
-        DP[2] = 2;
-        DP[3] = 4;
 
-        for (int j = 4; j < 12; j++) {
-            DP[j] = DP[j - 1] + DP[j - 2] + DP[j - 3];
+        // 1을 기준으로 i를 만드는 i-1의 개수
+        // 2를 기준으로 i를 만드는 i-2의 개수
+        // 3을 기준으로 i를 만드는 i-3의 개수
+        for (int i = 4; i < 12; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
-        for (int i = 1; i <= T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(DP[n]);
+        while (T-- > 0) {
+            int N = fr.nextInt();
+            sb.append(dp[N]).append("\n");
         }
+
+        System.out.println(sb.toString().trim());
+
 
     }
 
+
+
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
 }
