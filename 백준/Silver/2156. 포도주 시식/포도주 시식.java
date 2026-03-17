dc9ae604dddf8112fed21ac9a8fc45cff22c2d9d
CHANUK HEO <siai3647@gmail.com>
2026-03-17 12:56:09 +0900

    [Silver I] Title: 포도주 시식, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -3,44 +3,66 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        int N = fr.nextInt();
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
 
-        int[] arr = new int[n];
-        int[] dp = new int[n + 1];
-
-        // 포도주 테이블 초기화
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] =  fr.nextInt();
         }
 
-        if (n < 2) {
-            System.out.println(arr[0]);
-            return;
+        dp[1] = arr[1];
+
+        for (int i = 2; i <= N; i++) {
+            if (i == 2) {
+                dp[i] = arr[2] + arr[1];
+                continue;
+            }
+            dp[i] = Math.max(dp[i - 1], // 현재 i 번째 포도주를 먹지 않는 경우  
+                    Math.max(arr[i] + dp[i - 2],  // i번째 + 점프
+                            arr[i] + arr[i - 1] + dp[i - 3] // i-1, i + 점프
+                    )
+            );
         }
-
-        dp[1] = arr[0];
-        dp[2] = arr[0] + arr[1];
-
-        int max = dp[2];
-
-        for (int i = 2; i < n; i++) {
-            int cur = arr[i];
-            int pre = arr[i - 1];
-            int dpIndex = i + 1;
-            dp[dpIndex] = Math.max(cur + pre,
-                    Math.max(dp[dpIndex - 2] + cur, dp[dpIndex - 3] + cur + pre));
-            dp[dpIndex] = Math.max(max, dp[dpIndex]);
-            max = dp[dpIndex];
-        }
-
-        System.out.println(max);
-
-
-
-
-
+        System.out.println(dp[N]);
     }
 
+
+
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
 }
