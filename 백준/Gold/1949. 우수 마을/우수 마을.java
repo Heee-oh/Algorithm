dd15301f9fd663f0299ef83c242097fdb3666179
CHANUK HEO <siai3647@gmail.com>
2026-03-30 20:32:31 +0900

    [Gold II] Title: 우수 마을, Time: 148 ms, Memory: 18448 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.java
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -1,76 +1,127 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    static int[] populations;
+
+
     static int[][] dp;
-    static List<Integer>[] graph;
-    static boolean[] visited;
+    static int[] cnt;
 
-    static int n;
+    static int N;
+    static List<Integer>[] trees;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        populations = new int[n + 1];
-        graph = new ArrayList[n+1];
-        visited = new boolean[n + 1];
-        dp = new int[n + 1][2]; // 마을, 우수 선정 여부 (선정, 미선정)
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        N = fr.nextInt();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            populations[i] = Integer.parseInt(st.nextToken());
-            graph[i] = new ArrayList<>();
+        trees = new ArrayList[N+1];
+        dp = new int[N + 1][2];
+        cnt = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            cnt[i] = fr.nextInt();
+            trees[i] = new ArrayList<>();
         }
 
+        for (int i = 0; i < N - 1; i++) {
 
-        for (int i = 1; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int village1 = Integer.parseInt(st.nextToken());
-            int village2 = Integer.parseInt(st.nextToken());
-
-
-            graph[village1].add(village2);
-            graph[village2].add(village1);
+            int u = fr.nextInt();
+            int v = fr.nextInt();
+            trees[u].add(v);
+            trees[v].add(u);
         }
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= N; i++) {
             Arrays.fill(dp[i], -1);
+
         }
 
+        System.out.println(Math.max(dfs(1, 0, 0), dfs(1, 0, 1)));
 
-        System.out.println(Math.max(dfs(1,0, 1), dfs(1,0,0)));
 
+        System.out.println(sb.toString().trim());
     }
 
-    private static int dfs(int curVillage, int preVillage, int isBest) {
-        if (dp[curVillage][isBest] != -1) {
-            return dp[curVillage][isBest];
+
+    private static int dfs(int cur, int parent, int pSelect) {
+
+        if (dp[cur][pSelect] != -1) {
+            return dp[cur][pSelect];
         }
 
-        int sum = 0;
+        dp[cur][pSelect] = 0;
+        int tmp = pSelect == 0 ? 0 : cnt[cur];
+        for (int next : trees[cur]) {
+            if (parent == next) continue;
 
-        if (isBest == 1) {
-            sum = populations[curVillage];
-            for (int next : graph[curVillage]) {
-                if (next == preVillage) continue;
-                sum += dfs(next, curVillage, 0);
+            if (pSelect == 0) {
+                tmp += Math.max(dfs(next, cur, 0), dfs(next, cur, 1)  );
+            } else {
+                tmp += dfs(next, cur, 0);
+
             }
-        } else {
-            for (int next : graph[curVillage]) {
-                if (next == preVillage) continue;
-                sum += Math.max(dfs(next, curVillage, 0), dfs(next, curVillage, 1));
-            }
+
         }
 
-        dp[curVillage][isBest] = sum;
-
-        return dp[curVillage][isBest];
+        return dp[cur][pSelect] = tmp;
     }
 
+
+
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            long val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+    }
 }
