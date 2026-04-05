40c525a34efb10d9f4c4da7608b977874103b088
CHANUK HEO <siai3647@gmail.com>
2026-04-05 17:50:23 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15512 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,27 +1,26 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
     static int n;
     static int m;
     static StringBuilder sb = new StringBuilder();
     static StringBuilder answer = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        FastReader fr = new FastReader();
+        StringBuilder sb = new StringBuilder();
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        n = fr.nextInt();
+        m = fr.nextInt();
 
-        backtracking(1, 0);
+        dfs(1, 0);
 
-        System.out.print(answer.toString());
+        System.out.print(answer.toString().trim());
+
     }
-
-
-    private static void backtracking(int num, int depth) {
+    private static void dfs(int num, int depth) {
 
         if (depth == m) {
             sb.append("\n");
@@ -30,11 +29,66 @@ public class Main {
             return;
         }
         for (int i = num; i <= n; i++) {
-
             sb.append(i).append(" ");
-            backtracking(i, depth+1);
+            dfs(i, depth+1);
             sb.delete(sb.length() - 2, sb.length());
 
+        }
+    }
+
+
+    static class FastReader {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            long val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sign;
         }
     }
 }
