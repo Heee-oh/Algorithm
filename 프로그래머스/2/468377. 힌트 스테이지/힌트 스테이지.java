d6f412df5659ae83935df6a1f67a19017e4d47d9
CHANUK HEO <siai3647@gmail.com>
2026-04-07 21:09:04 +0900

    [level 2] Title: 힌트 스테이지, Time: 40.29 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    
+    static int n, min;
+    static List<boolean[]> list;
+    
+    public int solution(int[][] cost, int[][] hint) {
+        int answer = Integer.MAX_VALUE;
+        n = cost.length;
+
+        list = new ArrayList<>();
+    
+        dfs(0, new boolean[n]);
+       
+        
+        for (int i = 0; i < list.size(); i++) {
+            boolean[] buy = list.get(i);
+            int sum = 0;
+            
+            int[] coupon = new int[n+1];
+            
+            for (int stage = 0; stage < n; stage++) {    
+                int used = Math.min(coupon[stage + 1], n - 1);
+                sum += cost[stage][used];
+                
+                if (stage < n - 1 && buy[stage]) {
+                    sum += hint[stage][0];
+                    for (int k = 1; k < hint[stage].length; k++) {
+                        int hintN = hint[stage][k];
+                        coupon[hintN]++;
+                    }
+                }
+            }
+            
+            answer = Math.min(answer, sum);
+            
+        }
+      
+        
+        return answer;
+    }
+    
+    
+    
+    private void dfs(int stage, boolean[] buy) {
+        if (stage == n - 1) {
+            list.add(buy.clone());
+            return;
+        }
+
+        // 안 사는 경우
+        buy[stage] = false;
+        dfs(stage + 1, buy);
+
+        // 사는 경우
+        buy[stage] = true;
+        dfs(stage + 1, buy);
+        
+    }
+}
