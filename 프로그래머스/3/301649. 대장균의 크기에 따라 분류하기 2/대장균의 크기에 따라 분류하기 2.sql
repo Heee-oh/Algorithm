3fe3f1434440cd906db6e0da467b519a7259a907
CHANUK HEO <siai3647@gmail.com>
2026-04-11 18:40:54 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,26 +1,16 @@
--- 개체 크기 내림차순 정렬시
--- ID, 분류된 이름 (COLONY_NAME)
--- 개체 ID 에 대해 오름차순
--- 총 데이터 수 4의 배수, 같은 사이즈의 대장균 개체가 서로 다른 이름으로 분류되는 경우 X
+-- 코드를 작성해주세요
+-- 1 ~ 총데이터 / 4 개 상위 ?
 
+SELECT EC.ID, (CASE EC.COLONY_RANK
+                WHEN 1 THEN 'CRITICAL'
+                WHEN 2 THEN 'HIGH'
+                WHEN 3 THEN 'MEDIUM'
+                ELSE 'LOW'
+                END
+               ) AS COLONY_NAME
+FROM (
+    SELECT ID, NTILE(4) OVER (ORDER BY SIZE_OF_COLONY DESC) AS COLONY_RANK
+    FROM ECOLI_DATA
+) AS EC
+ORDER BY EC.ID 
 
-
-SELECT T.ID,(CASE 
-                WHEN T.SEQ <= (SELECT MAX(ID) 
-                            FROM ECOLI_DATA
-                           ) / 4 THEN 'CRITICAL'
-                WHEN T.SEQ <= (SELECT MAX(ID) 
-                            FROM ECOLI_DATA
-                           ) / 2 THEN 'HIGH'
-                WHEN T.SEQ <= (SELECT MAX(ID) 
-                            FROM ECOLI_DATA
-                           ) * 3 / 4 THEN 'MEDIUM'
-              ELSE 'LOW'
-            END
-           ) AS COLONY_NAME
-FROM (SELECT ID, ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS SEQ
-        FROM ECOLI_DATA
-        ORDER BY SIZE_OF_COLONY DESC
-     ) AS T
-     
-ORDER BY ID ASC
