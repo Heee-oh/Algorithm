df28275a1a8c5d7678e36724eb312aecf9091f23
CHANUK HEO <siai3647@gmail.com>
2026-04-18 18:34:49 +0900

    Time: 260 ms (69.38%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,10 @@
-SELECT IF(COUNT(*) = 1, NULL, MIN(e.salary)) as SecondHighestSalary 
-FROM (
-    SELECT DISTINCT(salary)
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 2
-) AS e
+SELECT MAX(salary) as SecondHighestSalary 
+FROM employee
+WHERE salary < (
+    SELECT salary 
+    FROM employee 
+    ORDER BY salary DESC 
+    LIMIT 1
+    )
 
 
