a7d4bac86001d1435e514ac9172df5e4efcae80d
CHANUK HEO <siai3647@gmail.com>
2026-05-23 22:30:18 +0900

    Time: 551 ms (66.42%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,7 +1,12 @@
 # 처음 로그인 여부
 # 그 다음날 로그인 여부
 
-SELECT ROUND(SUM(CASE WHEN DATE_ADD(t.first, INTERVAL 1 DAY) = a.event_date THEN 1 ELSE 0 END) / COUNT(DISTINCT a.player_id), 2) as fraction  
+SELECT ROUND(
+    SUM(
+        CASE WHEN DATE_ADD(t.first, INTERVAL 1 DAY) = a.event_date 
+            THEN 1 
+            ELSE 0 
+        END) / COUNT(DISTINCT a.player_id), 2) as fraction  
 FROM Activity a
 JOIN (
     SELECT player_id, MIN(event_date) as first
