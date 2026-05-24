f550664f42c80f64b9b50940f73f35de8397e02c
CHANUK HEO <siai3647@gmail.com>
2026-05-24 20:39:13 +0900

    Time: 371 ms (30.36%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,5 @@
-SELECT t.name
-FROM (
-    SELECT e2.id, e2.name
-    FROM Employee e1 
-    JOIN Employee e2 ON e1.managerId = e2.id
-    GROUP BY id, name
-    HAVING COUNT(e1.id) >= 5
-) as t
+SELECT e2.name
+FROM Employee e1 
+JOIN Employee e2 ON e1.managerId = e2.id
+GROUP BY e2.id, e2.name
+HAVING COUNT(e1.id) >= 5
