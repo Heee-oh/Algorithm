2c16fec65656f83107bcb2a3f5769805dd0c3de5
CHANUK HEO <siai3647@gmail.com>
2026-05-28 18:03:25 +0900

    [level 3] Title: 스티커 모으기(2), Time: 5.52 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,36 +1,43 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int sticker[]) {
-        int[] dp = new int[sticker.length];
         int answer = 0;
+        int n = sticker.length;
+        int[] dp0 = new int[sticker.length];
+        int[] dp1 = new int[sticker.length];
         
-        if (dp.length == 1) {
-            return sticker[0];
-        }
+        // 원형이기때문에 뜯어낼 경우와 안뜯어 낼경우 탐색 범위를 나눠야함 그래야 안전하게 가능
+        Arrays.fill(dp0, -1);
+        Arrays.fill(dp1, -1);
         
-        // 0부터 시작
-        dp[0] = sticker[0];
-        for (int i = 1; i < dp.length - 1; i++) {
-            if (i == 1) {
-                dp[i] = Math.max(sticker[i], dp[i - 1]);
-                continue;
+        
+        dp0[0] = sticker[0];
+        for (int i = 1; i < n - 1; i++) {
+            if (i - 2 < 0) {
+                dp0[i] = dp0[i-1];
+                
+            } else {
+                dp0[i] = Math.max(dp0[i-1], dp0[i-2] + sticker[i]);
             }
-            dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
         }
-        answer = dp[dp.length - 2];
-
-        dp[0] = 0;
         
-        // 1부터 시작
-        for (int i = 1; i < dp.length; i++) {
-            if (i == 1) {
-                dp[i] = Math.max(sticker[i], dp[i - 1]);
-                continue;
+        
+        dp1[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (i - 2 < 0) {
+                dp1[i] = Math.max(dp1[i-1], sticker[i]);
+                
+            } else {
+                dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
             }
-            dp[i] = Math.max(dp[i - 2] + sticker[i], dp[i - 1]);
         }
-   
-        answer = Math.max(answer, dp[dp.length - 1]);
-
+        
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, Math.max(dp0[i], dp1[i]));
+        }
+        
         return answer;
+        
     }
 }
