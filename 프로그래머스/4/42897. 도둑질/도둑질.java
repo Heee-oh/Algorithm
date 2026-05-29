380e40f3f46ea23f047b25a07c8b39eacfc4a8e8
CHANUK HEO <siai3647@gmail.com>
2026-05-29 10:25:28 +0900

    [level 4] Title: 도둑질, Time: 46.68 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -4,29 +4,20 @@ class Solution {
     public int solution(int[] money) {
         int n = money.length;
         
-        int[] dp0 = new int[n];
-        int[] dp1 = new int[n];
-        Arrays.fill(dp0, -1);
-        Arrays.fill(dp1, -1);
-        
-        // 0번 마을 털기
-        dp0[0] = money[0];
-        for (int i = 1; i < n-1; i++) {
-            if (i - 2 < 0) {
-                dp0[i] = dp0[i-1];
-            } else {
-                dp0[i] = Math.max(dp0[i-1], dp0[i-2] + money[i]);
-            }
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = dp[i][1] = -1;
         }
         
-        // 0번 마을을 안털고 1번마을 부터 
-        dp1[0] = 0;
-        dp1[1] = money[1];
+        dp[0][0] = dp[1][0] = money[0];
+        dp[0][1] = 0;
+        dp[1][1] = money[1];
         
         for (int i = 2; i < n; i++) {
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-2][0] + money[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][1] + money[i]);
         }
-
-        return Math.max(dp0[n-2], dp1[n-1]);
+        
+        return Math.max(dp[n-2][0], dp[n-1][1]);
     }
 }
