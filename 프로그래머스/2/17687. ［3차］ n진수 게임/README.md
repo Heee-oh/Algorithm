a467126258a30ee264e340e630f0146d384c3fa9
CHANUK HEO <siai3647@gmail.com>
2026-06-01 09:58:28 +0900

    [level 2] Title: [3차] n진수 게임, Time: 5.38 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687#) 
 
 ### 성능 요약
 
-메모리: 179 MB, 시간: 128.29 ms
+메모리: 65.8 MB, 시간: 5.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 01일 09:51:20
+2026년 06월 01일 09:58:26
 
 ### 문제 설명
 
--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,13 +1,16 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= 1000000; i++) {
+        
+        // 최악의 경우 16진수로 t = 1000개, m = 100, p = 100일 경우가 있기에
+        // 저 조건을 만족하는 안전한 수치만큼 번호 생성
+        for (int i = 0; i <= 30000; i++) {
             sb.append(Integer.toString(i, n));
         }
         String str = sb.toString();
         sb = new StringBuilder();
         int cnt = 0;
-        for (int i = p - 1; cnt < t; i+= m, cnt++) {
+        for (int i = p - 1; cnt < t ; i+= m, cnt++) {
             char c = str.charAt(i);
             if (Character.isAlphabetic(c)) {
                 c -= 32;
