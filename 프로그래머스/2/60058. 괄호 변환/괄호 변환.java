8375ef5ad80a512ac6d14284234309a9d99c7cf9
CHANUK HEO <siai3647@gmail.com>
2026-06-11 18:58:15 +0900

    [level 2] Title: 괄호 변환, Time: 7.66 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,92 @@
+import java.util.Stack;
+
+class Solution {
+    char[] arr;
+    
+    public String solution(String p) {
+        String answer = "";
+        arr = p.toCharArray();
+        
+        // 2. 올바른 괄호 문자열 변환 로직
+        return v(0, arr.length - 1);
+    }
+    
+    private String reverse(int s, int e) {
+        if (e == arr.length - 1) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = s; i <= e; i++) {
+            if (arr[i] == ')') arr[i] = '(';
+            else arr[i] = ')';
+            
+            sb.append(arr[i]);
+        }
+        
+        return sb.toString();
+    }
+    
+    private String v(int s, int e) {
+        if (s >= arr.length) {
+            return "";
+        }
+        
+        // 1. 올바른 괄호 문자열 판단
+        if (isAlright(s, e)) {
+            return charToString(s, e);
+        }
+        
+        int open = 0, close = 0;
+        for (int i = s; i <= e; i++) {
+            if (arr[i] == '(') open++;
+            else close++;
+            
+            // 3-1. u를 균형잡힌 괄호 문자열로 분리 
+            // u가 더이상 분리 불가 균형잡힌 괄호 문자열이여야함
+            if (open == close) {
+                String v = v(i+1, e);
+                
+                
+                if (isAlright(s, i)) {
+                    String u = charToString(s, i);
+                    return u + v;
+                }
+                
+                String reverseU = reverse(s + 1, i - 1);
+                return "(" + v + ")" + reverseU;
+            }
+        }
+        
+        return "";
+    }
+    
+    private boolean isAlright(int s, int e) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = s; i <= e; i++) {
+            char c = arr[i];
+            if (stack.isEmpty()) {
+                stack.push(c);
+                
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+                
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    private String charToString(int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s; i <= e; i++) {
+            sb.append(arr[i]);
+        }
+        
+        return sb.toString();
+    }
+}
