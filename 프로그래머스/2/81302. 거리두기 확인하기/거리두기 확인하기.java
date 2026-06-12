c01587ad7f222b7e82f5acee2e6ac75671a03e71
CHANUK HEO <siai3647@gmail.com>
2026-06-12 09:40:42 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.44 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+class Solution {
+    
+    int[] dr = {0, 0, 1, -1};
+    int[] dc = {1, -1, 0, 0};
+    
+    // 모든 정점의 거리값이 2를 초과하면 됌
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        Arrays.fill(answer, 1);
+        
+        for (int i = 0; i < places.length; i++) {
+            char[][] map = new char[5][5];
+            
+            for (int j = 0; j < 5; j++) {
+                map[j] = places[i][j].toCharArray();                
+            }
+            
+            boolean flag = false;
+            for (int r = 0; r < 5; r++) {
+                for (int c = 0; c < 5; c++) {
+                    if (map[r][c] == 'P') {
+                        if (!bfs(map, r, c)) {
+                            answer[i] = 0;
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if (flag) break;
+            }
+            
+        }
+        
+        
+        return answer;
+    }
+    
+    private boolean bfs(char[][] map, int r, int c) {
+        boolean[][] visited = new boolean[map.length][map[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {r, c, 0});
+        visited[r][c] = true;
+        
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            
+            if (cur[2] >= 3) {
+                break;
+            }
+            
+            for (int d = 0; d < 4; d++) {
+                int nr = cur[0] + dr[d];
+                int nc = cur[1] + dc[d];
+                
+                if (nr < 0 || nr >= map.length 
+                    || nc < 0 || nc >= map[0].length 
+                    || visited[nr][nc] || map[nr][nc] == 'X') continue;
+                    
+                
+                // 도달 거리가 2이하이면 거리두지 않은 판단
+                if (map[nr][nc] == 'P' && cur[2] + 1 <= 2) {
+                    return false;
+                }
+                
+                visited[nr][nc] = true;
+                q.offer(new int[] {nr, nc, cur[2] + 1});
+                
+                
+                
+            }
+        }
+        
+        
+        
+        return true;
+        
+        
+    }
+}
