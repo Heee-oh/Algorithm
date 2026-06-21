8e6d19c06ee2ab3ea7a7ddca99a09b84a510d604
CHANUK HEO <siai3647@gmail.com>
2026-06-21 18:37:14 +0900

    Time: 380 ms (64.93%), Space: 0B (100%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,24 @@
+WITH t AS (
+    SELECT LAG(id) OVER(ORDER BY id asc) AS prev, id, LEAD(id) OVER (ORDER BY id ASC) AS post 
+        FROM Stadium
+        WHERE people >= 100
+)
+
+SELECT *
+FROM stadium
+WHERE id IN (
+    (
+        SELECT t.prev
+        FROM t
+        WHERE post - prev = 2
+    ) UNION ALL (
+        SELECT t.id
+        FROM t
+        WHERE post - prev = 2
+    ) UNION ALL (
+        SELECT t.post
+        FROM t
+        WHERE post - prev = 2
+    )
+)
+ORDER BY visit_date
