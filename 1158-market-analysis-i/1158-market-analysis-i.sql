a6cb21ae4ae45d861486cc7c2a799b81823ce5a5
CHANUK HEO <siai3647@gmail.com>
2026-06-29 18:37:20 +0900

    Time: 1495 ms (15.95%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,7 +1,7 @@
 # 각 유저별 가입일(join_date)과 2019년에 구매자(buyer)로서 주문한 횟수
 # 순서 상관 X
 
-SELECT user_id as 'buyer_id', join_date, IFNULL(SUM(YEAR(o.order_date) = 2019), 0) AS 'orders_in_2019'
+SELECT user_id as 'buyer_id', join_date, SUM(IF(o.buyer_id IS NULL,0, YEAR(o.order_date) = 2019)) AS 'orders_in_2019'
 FROM Users u
 LEFT JOIN Orders o ON u.user_id = o.buyer_id 
 GROUP BY user_id, join_date
